ffb40fee5f870fa02f1428c66b3503ded9203a21
VasilisMich <VasilisMich789@gmail.com>
2023-10-20 18:40:44 +0300

    first commit

--- src/routes/vasilis/+page.svelte
+++ src/routes/vasilis/+page.svelte
@@ -1,5 +1,13 @@
 <script>
+    import { onMount } from "svelte";
+    import { universisGet } from "$lib/dataService";
 
+    onMount(async () => {
+
+        let grades = await universisGet("students/me/grades");
+
+        let subjects = await universisGet("students/me/courses");
+    }
 </script>
 
 <p>Hello Vasilis</p>
