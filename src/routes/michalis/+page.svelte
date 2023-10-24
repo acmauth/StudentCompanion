52b0ed2dde9e47e421c383a2870564ab7360d57d
RogZero <program0zero@gmail.com>
2023-10-24 14:34:44 +0300

    Fix: Password with '#'

--- src/routes/michalis/+page.svelte
+++ src/routes/michalis/+page.svelte
@@ -1,15 +1,98 @@
 <script>
     import { onMount } from "svelte";
     import { universisGet } from "$lib/dataService";
+ 
 
-    let username = "";
+    /**
+	 * @type {any[]}
+	 */
+    let unpassed_courses = [];
 
+    let not_passed_all_courses = false; // If false then, the student has passed all of their courses
+
+    // some multitudes
+    let unpassed_courses_multitude = 0;
+    let passed_courses_multitude = 0;
+    let courses_multitude = 0; // unpassed + passed
+
+    // Grades sum
+    let sum_unpassed_courses = 0; 
+    let sum_passed_courses = 0;
+    let degree_grade = 0;
+    let degree_grade_string = "";
+        
     onMount(async () => {
-        let personalData = await universisGet("users/me");
-        username = personalData.name;
-    });
-    
+        // Fetch a list of the current student's courses
+        let courses = (await universisGet("students/me/courses?$top=-1")).value;
+
+        // Loop through the courses. If the current course is
+        // not passed) change the not_passed_all_courses bool to true and push the courses to the unpassed_courses list
+        // passed) add the grade to the sum of the passed courses grade
+        for (const course of courses)
+        {
+            if (course.isPassed == 0)
+            {
+                unpassed_courses_multitude++;
+                not_passed_all_courses = true;
+                unpassed_courses.push({title: course.courseTitle, grade: 0});
+            }
+            else
+            {
+                passed_courses_multitude++;
+                sum_passed_courses += course.grade;
+            }
+        }
+
+        // calculate pre existing degree grade with a two digit percision
+        degree_grade = sum_passed_courses/passed_courses_multitude;
+        degree_grade_string = String(degree_grade.toFixed(2));
+    });   
+
+    // If the value of an unpassed course is different than 0, the divisor of the degree grade, rises by 1
+    // Whenever there are changes in the unpassed course grades, the average is recalculated
+    // taking count of the new grade assumption
+
+    function updateGrades() 
+    {
+        unpassed_courses_multitude = 0;
+        sum_unpassed_courses = 0
+
+        for (const course of unpassed_courses)
+        {
+            if (course.grade != 0)
+                unpassed_courses_multitude++;
+
+            sum_unpassed_courses += course.grade;
+        }
+
+        courses_multitude = unpassed_courses_multitude + passed_courses_multitude; // courses + inputed_courses multitude
+        degree_grade = ((sum_unpassed_courses+sum_passed_courses)/courses_multitude);
+        degree_grade_string = String(degree_grade.toFixed(2));
+	}
 </script>
 
+    <!-- Check if there are any unpassed courses and if so, show them in the screen and ask for input -->
+    {#if not_passed_all_courses}
+    <h2>To calculate your degreed grade, you must first give a prediction to your unpassed courses grades (0-1):</h2>
+    {#each unpassed_courses as course}
+    <div class="my-div">
+        <p>{course.title}</p>
+        <input type="range" bind:value={course.grade} min="0" max="1" step="0.01" on:input={updateGrades} >
+        <input type="number" bind:value={course.grade} min="0" max="1" step="0.01" on:input={updateGrades} >
+    </div>
+    {/each}
+    {/if}
 
-<p>Hello {username}</p>
+    <!-- Show the Degree grade in the screen -->
+    <h2>Degree grade: {degree_grade_string}</h2>
+
+
+
+<style>
+    /* Apply padding to the my-div class */
+    .my-div {
+    padding: 10px; /* Adjust the value to control the amount of padding */
+    border: 1px solid #000; /* Optional: add a border for visualization */
+    margin: 5px; /* Optional: add margin to create space between the divs */
+}
+</style>
