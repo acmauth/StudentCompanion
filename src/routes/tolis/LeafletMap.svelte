56539b99908cc5ecfbdaaa86203f4707fc0e938c
TolisSth <tolishalis@gmail.com>
2023-11-01 21:44:28 +0200

    Moved files from lib to personal workplace

---
+++ src/routes/tolis/LeafletMap.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+    import { browser } from '$app/environment';
+
+    let mapElement;
+    let map;
+
+    onMount(async () => {
+        if(browser) {
+            const leaflet = await import('leaflet');
+
+            // This part of the code sets the viewport, what area the user will be greeted with
+            map = leaflet.map(mapElement).setView([40.63182425082954, 22.959049527401312], 15);
+
+            leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            // Key locations markers 
+            leaflet.marker([40.63182425082954, 22.959049527401312]).addTo(map)
+                .bindPopup('Main AUTh Campus')
+                .openPopup();
+        }
+    });
+
+    onDestroy(async () => {
+        if(map) {
+            console.log('Unloading Leaflet map.');
+            map.remove();
+        }
+    });
+</script>
+
+
+<main>
+    <div bind:this={mapElement}></div>
+</main>
+
+<style>
+    @import 'leaflet/dist/leaflet.css';
+    main div {
+        height: 800px;
+    }
+</style>
