e3e8c4316916e747880c66f652609feadcffc204
Dimitra Angelidou <dangelidou@csd.auth.gr>
2023-11-02 19:03:29 +0200

    deleted express server - adjusted the svelte code

--- src/routes/cafeteriaService/+server.ts
+++ src/routes/cafeteriaService/+server.ts
@@ -1,11 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from '@sveltejs/kit';
+import axios from 'axios';
+import cheerio from 'cheerio';
+let scrapedHTML = '';
 
 export const GET: RequestHandler = async ({ url }) => {
-    
-    // data = ... 
-    // getting the data from the cafeteria website
+    try {
+        // data = ... 
+        // getting the data from the cafeteria website
+        const response = await axios.get('https://www.auth.gr/weekly-menu/');
+        const htmlContent = response.data;
+        const $ = cheerio.load(htmlContent);
 
-	return new Response(JSON.stringify(data));
+        const days = $('.kt-blocks-accordion-title');
+        const dailyMenus = $('.kt-accordion-panel-inner');
+        dailyMenus.each(function (idx, el) {
+            scrapedHTML += $(days[idx]).html();
+            scrapedHTML += $(el).html();
+        });
+    } catch (error) {
+        console.error('Error while scraping data:', error);
+    }
+    return new Response(JSON.stringify(scrapedHTML));
 };
--- src/routes/dimitra/+page.svelte
+++ src/routes/dimitra/+page.svelte
@@ -1,28 +1,21 @@
 <script>
 	import { onMount } from 'svelte';
-	import axios from 'axios';
-	import cheerio from 'cheerio';
 
-	let scrapedHTML = '';
+	/**
+	 * @type {any}
+	 */
+	let cafeteriaData;
 
 	onMount(async () => {
-		try {
-			const targetURL = 'http://localhost:3000/proxy?url=https://www.auth.gr/weekly-menu/';
-			const response = await axios.get(targetURL);
-			const htmlContent = response.data;
-			const $ = cheerio.load(htmlContent);
+		const response = await fetch('/cafeteriaService', { method: 'GET' });
 
-			const days = $('.kt-blocks-accordion-title');
-			const dailyMenus = $('.kt-accordion-panel-inner');
-			dailyMenus.each(function (idx, el) {
-				scrapedHTML += $(days[idx]).html();
-				scrapedHTML += $(el).html();
-			});
-		} catch (error) {
-			console.error('Error while scraping data:', error);
+		if (response.ok) {
+			cafeteriaData = await response.json();
+		} else {
+			console.error('Error fetching cafeteria data:', response.statusText);
 		}
 	});
 </script>
 
 <h1>Μενού Λέσχης</h1>
-<div>{@html scrapedHTML}</div>
+<div>{@html cafeteriaData}</div>
