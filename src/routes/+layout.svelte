ffd2b9f9b2d386693575c2717cb4c1084ad8aca1
Neron-png <neronmkp@csd.auth.gr>
2023-11-03 23:01:07 +0200

    Initial gui commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { setupIonicBase } from 'ionic-svelte';
+
+	/* Call Ionic's setup routine */
+	setupIonicBase();
+
+	/* Import all components - or do partial loading - see below */
+	import 'ionic-svelte/components/all';
+
+	/* Theme variables */
+	import '../theme/variables.css';
+
+
+
+</script>
+
+<ion-app>
+	<slot />
+</ion-app>
---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: isProduction
+			? adapterStatic({
+				pages: 'build',
+				assets: 'build',
+				fallback: 'index.html',
+				precompress: false
+			})
+			: adapterAuto()
+	}
+};
+
+export default config;
