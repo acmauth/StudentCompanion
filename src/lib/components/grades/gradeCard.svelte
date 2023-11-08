4d26597bdebb58328d997189d56b0970e801efde
Konstantinos Agathopoulos <kostaga203@gmail.com>
2023-11-08 02:29:41 +0200

    changed to grid

--- src/lib/components/grades/gradeCard.svelte
+++ src/lib/components/grades/gradeCard.svelte
@@ -8,30 +8,40 @@
 </script>
 
 <ion-card href="/" class="ion-padding">
-	<div class="card">
-		<ion-card-header>
-			<ion-card-title class="ion-padding-bottom">{subject.courseTitle}</ion-card-title>
+	<ion-grid>
+		<ion-row class="ion-justify-content-start">
+			<ion-col class="ion-no-padding" size-md="10" size-lg="8" size-xl="6">
+				<ion-card-header>
+					<ion-card-title class="ion-padding-bottom">{subject.courseTitle}</ion-card-title>
 
-			<div class="icons">
-				<ion-icon icon={allIonicIcons.flagOutline} />
-				<ion-card-subtitle>{subject.course}</ion-card-subtitle>
-			</div>
+					<div class="icons">
+						<ion-icon icon={allIonicIcons.flagOutline} />
+						<ion-card-subtitle>{subject.course}</ion-card-subtitle>
+					</div>
 
-			<div class="icons">
-				<ion-icon icon={allIonicIcons.personOutline} />
-				<ion-card-subtitle>Nikolaos Tsitsas</ion-card-subtitle>
-			</div>
-		</ion-card-header>
+					<div class="icons">
+						<ion-icon icon={allIonicIcons.personOutline} />
+						<ion-card-subtitle>Nikolaos Tsitsas</ion-card-subtitle>
+					</div>
+				</ion-card-header>
+			</ion-col>
 
-		<ion-card-content>
-			{#if subject.grade !== null}
-				<ion-text color="success"><h2>{subject.formattedGrade}</h2></ion-text>
-			{:else}
-				<p>Ungraded</p>
-			{/if}
-		</ion-card-content>
-	</div>
+			<ion-col size="20">
+				<ion-card-content class="ion-text-center">
+					{#if subject.grade !== null}
+						<ion-text color="success">
+							<h2>{subject.formattedGrade}</h2>
+						</ion-text>
+					{:else}
+						<p>Ungraded</p>
+					{/if}
+				</ion-card-content>
+			</ion-col>
+		</ion-row>
+	</ion-grid>
 </ion-card>
+
+
 
 <style>
 	.card {
