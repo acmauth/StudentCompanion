6b15337776f3a1fb34c0204dbc263e7733ad9606
VasilisMicha <VasilisMich789@gmail.com>
2023-11-11 14:51:23 +0200

    fixed instructors appearing twice

--- src/routes/vasilis/[courseInfo]/+page.svelte
+++ src/routes/vasilis/[courseInfo]/+page.svelte
@@ -21,6 +21,8 @@
         // Getting an array with user's registered courses and information about them
         registrations = (await universisGet("students/me/Registrations?$expand=classes($expand=courseType($expand=locale),courseClass($expand=course($expand=locale),instructors($expand=instructor($select=InstructorSummary))))&$top=-1&$skip=0&$count=false")).value;
 
+        console.log(registrations);
+
         // Finding the course and storing informations about it in variables
         for (const course of courses) {
             if (course.course === courseInfo){
@@ -34,17 +36,23 @@
         // Getting the instructors' names for the course from the registrations
         let familyName = [];
         let givenName = [];
+        let flag = false;
         for (const semester of registrations){
             for (const classes of semester.classes){
                 if (classes.course === courseInfo){
                     for (const instructor of classes.courseClass.instructors){
                         courseInstructors.push({familyName: instructor.instructor.familyName, givenName: instructor.instructor.givenName});
                     }
+                    flag = true;
                 }
+                if (flag)
+                    break;
             }
+            if (flag)
+                break;
         }
 
-
+        console.log(courseInstructors);
     });
     
 
