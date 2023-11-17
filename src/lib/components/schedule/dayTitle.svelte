ab6b5eca1f39d1ccf7a6982b75f6076625dd5aaa
Christos Balaktsis <balaktsis@gmail.com>
2023-11-17 18:54:30 +0200

    Minor fixes

--- src/lib/components/schedule/dayTitle.svelte
+++ src/lib/components/schedule/dayTitle.svelte
@@ -32,10 +32,10 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <ion-grid id="{day.toLowerCase()}" on:click={setActiveDay}>
 	<ion-row class="ion-align-items-center ion-justify-content-center" style="padding: 3%; font-size: 200%">
-		{#if $activeDay != day.toLowerCase() }
-			<ion-icon id="icon" icon={allIonicIcons.ellipseOutline}/>
-		{:else}
+		{#if $activeDay == day.toLowerCase() || day.toLowerCase() == "mon" && ($activeDay == "sat" || $activeDay == "sun") }
 			<ion-icon id="icon" icon={allIonicIcons.ellipse}/>
+		{:else}
+			<ion-icon id="icon" icon={allIonicIcons.ellipseOutline}/>
 		{/if}
 	</ion-row >
 	<ion-row class="ion-align-items-center ion-justify-content-center">
