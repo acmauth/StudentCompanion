1777de801e7ffc1bfb22745712cf5d71e73f6026
Neron png <neronios2002@gmail.com>
2023-11-21 22:36:34 +0200

    Update taskStore.ts

--- src/lib/components/schedule/task/taskStore.ts
+++ src/lib/components/schedule/task/taskStore.ts
@@ -1,5 +1,5 @@
 import type { Writable } from "svelte/store";
 import type { TaskItem } from "./TaskItem";
-import { persisted } from 'svelte-local-storage-store'
+import { persisted } from 'svelte-persisted-store'
 
-export const taskStore: Writable<TaskItem[]> = persisted("taskItem", []);
+export const taskStore = persisted("taskItem", []);
