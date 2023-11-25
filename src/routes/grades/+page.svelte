e3a539772a5d43e3605fddbb24b430f42d3d3681
Konstantinos Agathopoulos <59094550+Kostaga@users.noreply.github.com>
2023-11-25 21:57:44 +0200

    Dev (#10)
    * working on grades page
    
    * working on grades page
    
    * changed to grid
    
    * fixed some comments from pull request
    
    * still working on grades
    
    * still working on grades (#3)
    
    * working on personal info
    
    * finished with personal info for now
    
    * finished grades page almost (#6)
    
    ---------
    
    Co-authored-by: Christos Balaktsis <mpalaktsc@csd.auth.gr>

--- src/routes/grades/+page.svelte
+++ src/routes/grades/+page.svelte
@@ -1,10 +1,29 @@
 <script>
 	import Grades from '$lib/components/grades/grades.svelte';
+	import * as allIonicIcons from 'ionicons/icons';
+	import  courseBySemester  from '$lib/components/grades/grades.svelte';
+
+let searchQuery = '';
+
+	/**
+	 * @param {{ target: { value: string; }; }} event
+	 */
+	
+	function handleChange(event) {
+		searchQuery = event.target.value;
+	}
+
+
 </script>
 
 <ion-page style="overflow-y: auto;">
-	
-		<Grades />
+  <ion-nav-title>
+			<ion-item-divider sticky>
+				<ion-searchbar on:ionInput={handleChange} inputmode="text" show-clear-button="always" placeholder="Αναζήτηση Μαθημάτων"></ion-searchbar>
+			</ion-item-divider>
+	</ion-nav-title>
+  
+		<Grades searchQuery = {searchQuery}  />
+  
 
 </ion-page>
-
