f3f3e4ccd3709e82cc23dbee01f81f9378b4cea9
Neron-png <neronmkp@csd.auth.gr>
2023-11-26 01:02:40 +0200

    Intermediate commit

---
+++ src/routes/notifications/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+
+    import { Capacitor } from '@capacitor/core';
+    
+</script>
+
+<ion-header translucent={Capacitor.getPlatform() === 'ios'} mode="ios">
+        <ion-toolbar mode={Capacitor.getPlatform() != 'ios' ? 'md': undefined}>
+          <ion-title>This is a title</ion-title>
+        </ion-toolbar>
+</ion-header>
+      
+<ion-content fullscreen={true}>
+    <ion-header collapse="condense" mode="ios">
+        <ion-toolbar mode={Capacitor.getPlatform() != 'ios' ? 'md': undefined}>
+            <ion-title size="large">title</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    
+</ion-content>
+
+
+<style>
+
+</style>
