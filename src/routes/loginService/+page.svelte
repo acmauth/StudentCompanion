7b2ac92815b4e863d9e586332855844707033ccf
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2023-11-26 01:10:11 +0200

    Login screen (#12)
    * UI login screen
    
    * Added svg
    
    * Small changes
    
    * Merged functionality from prev commit'
    
    * Removed unnecessary ion-page
    
    ---------
    
    Co-authored-by: Neron-png <neronmkp@csd.auth.gr>

--- src/routes/loginService/+page.svelte
+++ src/routes/loginService/+page.svelte
@@ -1,47 +1,12 @@
-<script lang='ts'>
-    import { tokenGrab } from "$lib/universisAuthentication/tokenGeneratorWorker"
-    import { userCreds, userTokens } from "../../stores/credentials.store";
-    import userInfoStore from "../../stores/userinfo.store";
-    import { goto } from '$app/navigation';
-    import { elearningFetchNewToken } from "$lib/elearningAuthentication/elearningDataService";
-    import { IonButton } from "@ionic/core/components/ion-button";
-    import { getUniversisToken, getElearningToken} from "./helpers"
-    import { invalidateAuth } from "$lib/authentication/authValidator";
 
-    let username = '';
-    let password = '';
-    let outputMessage = ''
-
-    async function submit(){
-
-        let universisOutput = await getUniversisToken(username, password);
-        let elearningOutput = await getElearningToken(username, password);
-        
-        outputMessage = universisOutput + " " + elearningOutput;
-        goto("/");
-
-    }
-
-    function logOut(){
-        invalidateAuth();
-        goto("/");
-    }
-
+<script>
+	import Login from '$lib/components/loginService/login.svelte';
 </script>
 
-<h1>Login</h1>
-
-<form>
-    <label>
-        Username:
-        <input type="text" bind:value={username} />
-    </label>
-    <label>
-        Password:
-        <input type="password" bind:value={password} />
-    </label>
-    <button type="submit" on:click={submit}>Submit</button>
-</form>
-<p>{outputMessage}</p>
-
-<ion-button on:click={logOut}>Log out</ion-button>
+<ion-page>
+	<Login />
+</ion-page>
+  
+  
+  
+  
