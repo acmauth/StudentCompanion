7575f8be42eeb3db212c9eaefe3f801c4fd80be0
Neron-png <neronmkp@csd.auth.gr>
2023-11-25 21:53:51 +0200

    Implemented the new authentication pipeline into gui

--- src/routes/loginService/+page.svelte
+++ src/routes/loginService/+page.svelte
@@ -1,44 +1,24 @@
 <script lang='ts'>
-    import { tokenGrab } from "$lib/authentication/tokenGeneratorWorker"
-    import { userCreds } from "$lib/authentication/authStore";
+    import { tokenGrab } from "$lib/universisAuthentication/tokenGeneratorWorker"
+    import { userCreds, userTokens } from "../../stores/credentials.store";
+    import userInfoStore from "../../stores/userinfo.store";
     import { goto } from '$app/navigation';
-    import {validateAuth, invalidateAuth} from "$lib/authentication/authValidator";
-	import { IonButton } from "@ionic/core/components/ion-button";
-    
-    // This is a simple login page that gets a user token and stores it in the browser.
-
-    // export const load: PageLoad = ({ params }) => {
-	// return {
-	// 	post: {
-	// 		title: `Title for ${params.slug} goes here`,
-	// 		content: `Content for ${params.slug} goes here`,
-	// 	    },
-	//     };
-    // };
+    import { elearningFetchNewToken } from "$lib/elearningAuthentication/elearningDataService";
+    import { IonButton } from "@ionic/core/components/ion-button";
+    import { getUniversisToken, getElearningToken} from "./helpers.ts"
 
     let username = '';
     let password = '';
     let outputMessage = ''
 
     async function submit(){
-        // Using the username and password, we're calling the tokenGrab function to get an authentication token.
-        
-        // URL-encode the password
-        const encodedPassword = encodeURIComponent(password);
 
-        const response = await tokenGrab(username, encodedPassword);
-        if (response.error){
-            outputMessage = response.error;
-        }
-        else {
-            userCreds.set({
-                username: username,
-                password: password,
-                token: response.token
-            });
-            goto("/");
-            outputMessage = "Login successful!"
-        }
+        let universisOutput = await getUniversisToken(username, password);
+        let elearningOutput = await getElearningToken(username, password);
+        
+        outputMessage = universisOutput + " " + elearningOutput;
+        goto("/");
+
     }
 
     function logOut(){
