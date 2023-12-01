b7683a8e97b16962e5e7ce665ed9199ebd13c22b
Neron-png <neronmkp@csd.auth.gr>
2023-12-01 12:44:54 +0200

    small fix in +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
-	goto('home');
+	import { onMount } from 'svelte';
+	
+    onMount(async () => {
+        goto('home');
+    });
 
 </script>
