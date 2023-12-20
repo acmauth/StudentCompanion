06608a5b966fe0455c6b74bd4bb094bffcb2aeaa
Neron png <neronmkp@csd.auth.gr>
2023-12-20 19:58:07 +0200

    Added colours css properties and implemented the AppCard component to replace cards

---
+++ src/lib/components/shared/AppCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type {Colour} from "$types/colours"
+    export let colour: Colour = "default";
+    export let padding: boolean = false;
+    export let innerClass = $$props.class;
+    export let onClick = () => {}
+
+    let paddingstr = padding ? "padding: 1rem;" : "";
+
+</script>
+
+{#if colour === "default"}
+    <ion-card class="{innerClass}" on:click={onClick} aria-hidden>
+        <slot/>
+    </ion-card>
+{:else}
+    <ion-card class="{innerClass}" on:click={onClick} aria-hidden style="--background: var(--app-color-{colour})!important; --color: var(--app-color-{colour}-dark);{paddingstr}">
+        <slot/>
+    </ion-card>
+{/if}
---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import IonPage from "ionic-svelte/components/IonPage.svelte";
+    import AppCard from "$shared/AppCard.svelte";
+
+</script>
+
+<IonPage>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title>Home</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        
+        <AppCard colour="primary" padding>
+            <h1>This is a test</h1>
+            <ion-avatar>
+                <ion-skeleton-text animated style="width: 100%; height: 100%;"></ion-skeleton-text>
+            </ion-avatar>
+        </AppCard>
+        
+        <AppCard colour="orange">
+            <h1>This is a test</h1>
+        </AppCard>
+        
+        <AppCard colour="green">
+            <h1>This is a test</h1>
+        </AppCard>
+        
+        <AppCard colour="purple">
+            <h1>This is a test</h1>
+        </AppCard>
+        
+        <AppCard>
+            <h1>This is a test</h1>
+        </AppCard>
+    </ion-content>
+</IonPage>
