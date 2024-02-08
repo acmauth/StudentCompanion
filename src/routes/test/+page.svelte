6c26bebedf9669a9c3f0d5905cf0f105d3deb354
Neron png <neronmkp@csd.auth.gr>
2024-02-08 18:28:39 +0200

    Moved the Flipper, added example in /test/

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,54 +1,14 @@
 <script>
-	import { IonContent } from "@ionic/core/components/ion-content";
-    import AppCard from "$components/shared/AppCard.svelte";
-    import FlipCard from "./flipCard.svelte";
+    import Flipper from "$components/shared/Flipper.svelte";
+    import TestComponentA from "./testComponentA.svelte";
+	import TestComponentB from "./testComponentB.svelte";
 
 </script>
 
 <ion-content>
-    <FlipCard reactToHeight>
-        <AppCard slot="front">
-            <ion-card-header>
-                <ion-card-subtitle>Student companion (test) homepage</ion-card-subtitle>
-            </ion-card-header>
-            <ion-text>Homepage</ion-text>
-            <ion-card-content>
-                Let's list here our quick page links, before we get navigation figured out
-            </ion-card-content>
-            <ion-button href="/loginService">Log in/Log out</ion-button>
-        </AppCard>
-        <AppCard colour="orange" slot="back">
-            <ion-card-header>
-                <ion-card-subtitle>SECONDDDDD Student companion (test) homepage</ion-card-subtitle>
-            </ion-card-header>
-            <ion-button href="/loginservice">Homepage</ion-button>
-            <ion-card-content>
-                Let's list here our quick page links, before we get navigation figured out
-            </ion-card-content>
-            <ion-button href="/loginService">Log in/Log out</ion-button>
-        </AppCard>
-    </FlipCard>
-    <FlipCard reactToHeight>
-        <AppCard slot="front">
-            <ion-card-header>
-                <ion-card-subtitle>Student companion (test) homepage</ion-card-subtitle>
-            </ion-card-header>
-            <ion-text>Homepage</ion-text>
-            <ion-card-content>
-                Let's list here our quick page links, before we get navigation figured out
-            </ion-card-content>
-            <ion-button href="/loginService">Log in/Log out</ion-button>
-        </AppCard>
-        <AppCard colour="orange" slot="back">
-            <ion-card-header>
-                <ion-card-subtitle>SECONDDDDD Student companion (test) homepage</ion-card-subtitle>
-            </ion-card-header>
-            <ion-button href="/loginservice">Homepage</ion-button>
-            <ion-card-content>
-                Let's list here our quick page links, before we get navigation figured out
-            </ion-card-content>
-            <ion-button href="/loginService">Log in/Log out</ion-button>
-        </AppCard>
-    </FlipCard>
+    <Flipper reactToHeight>
+        <TestComponentA slot="front"/>
+        <TestComponentB slot="back"/>
+    </Flipper>
 </ion-content>
 
