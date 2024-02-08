f76c935be1a2ee189f2a24badc1e722f3da56051
Neron png <neronmkp@csd.auth.gr>
2024-02-08 18:08:49 +0200

    Finished logic for flipping cards, menei genikeysh

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,60 +1,12 @@
 <script>
+	import { IonContent } from "@ionic/core/components/ion-content";
     import FlipCard from "./flipCard.svelte";
 
 </script>
 
-<ion-page style="overflow-y: auto;">
+<ion-content>
+    <FlipCard/>
+    <FlipCard reactToHeight/>
+    <FlipCard/>
+</ion-content>
 
-    <FlipCard>
-
-    </FlipCard>
-
-	<!-- <ion-card>
-		<ion-card-header>
-			<ion-card-subtitle>Student companion (test) homepage</ion-card-subtitle>
-		</ion-card-header>
-		<ion-button href="/homepage">Homepage</ion-button>
-		<ion-card-content>
-			Let's list here our quick page links, before we get navigation figured out
-		</ion-card-content>
-		<ion-button href="/loginService">Log in/Log out</ion-button>
-	</ion-card>
-
-	<ion-card>
-		<ion-card-header>
-			<ion-card-subtitle>Grades Page</ion-card-subtitle>
-		</ion-card-header>
-		<ion-button href="/grades">Grades</ion-button>
-	</ion-card>
-
-<ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Personal Info Page</ion-card-subtitle>
-	</ion-card-header>
-	<ion-button href="/personalInfo">Personal Info</ion-button>
-</ion-card>
-
-<ion-card>
-	<ion-button href="/schedule">Schedule</ion-button>
-	<ion-button href="/notifications">Notifications</ion-button>
-</ion-card>
-
-<ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Degree Grade</ion-card-subtitle>
-	</ion-card-header>
-	<ion-button href="/degreeGrade">Calculate Degree Grade</ion-button>
-</ion-card>
-
-
-	<ion-card>
-		<ion-card-header>
-			<ion-card-subtitle>Restaurant Menu</ion-card-subtitle>
-		</ion-card-header>
-		<ion-button href="/menu">Weekly Menu</ion-button>
-	</ion-card>
-
-	<ion-card>
-		<ion-button href="/quickLinks">Quick Links</ion-button>
-	</ion-card> -->
-</ion-page>
--- src/routes/test/flipCard.svelte
+++ src/routes/test/flipCard.svelte
@@ -1,37 +1,60 @@
-<script>
+<script lang="ts">
     import AppCard from "$components/shared/AppCard.svelte";
-
+    export let reactToHeight: boolean = false;
+    
+    // The flip class is what we use to toggle the flip effect.
     let flipClass = false;
-    function flip() {
-        flipClass = !flipClass;
+
+    // We need to keep track of the flip container and the front
+    // and back children so we can update the height of the flip card.
+    let flipContainer: HTMLElement;
+    let frontChild: HTMLElement;
+    let backChild: HTMLElement;
+
+    // This function updates the height of the flip card when it's toggled.
+    const updateHeight = (status: Boolean) => {
+        // Important, this code will run once before the flip container is defined, so we need to check it exists.
+        if (flipContainer && frontChild && backChild) {
+            // The height relative to the rest of the page is dictated by the main container, so we 
+            // set it's height to the height of the front or back child, depending on the status of the flip.
+            // So if we're flipped, use the back child, otherwise the front
+            flipContainer.style.height = status ? `${backChild.clientHeight}px` : `${frontChild.clientHeight}px`;
+        }
     }
+
+    // When the flip class changes, update the height of the flip card.
+    // Check if we should update the height of the flip card when it's toggled.
+    // Sometimes we don't want to do this, that's why it's optional.
+    $: if (reactToHeight) updateHeight(flipClass);
 
 </script>
 
-<!-- <AppCard colour="primary">
-    <h1>Flip Card</h1>
-    <p>Click the button to flip the card</p>
-    <button on:click>Click me</button>
-</AppCard> -->
 
-<div on:click={() => flipClass = !flipClass} class="flip-container" class:flipClass={flipClass} aria-hidden>
-    <div class="flipper clearfix">
-      <div class="front card item-text-wrapped">
-        <div class="top">
-        </div>
-        <div class="icon-holder">A</div>
-        <div class="padding">
-          <p>Front side text</p>
-        </div>
+<div on:click={() => flipClass = !flipClass} class="flip-container" class:flipClass={flipClass} aria-hidden bind:this={flipContainer}>
+    <div class="flipper">
+      <div class="front" bind:this={frontChild}>
+        <AppCard>
+            <ion-card-header>
+                <ion-card-subtitle>Student companion (test) homepage</ion-card-subtitle>
+            </ion-card-header>
+            <ion-text>Homepage</ion-text>
+            <ion-card-content>
+                Let's list here our quick page links, before we get navigation figured out
+            </ion-card-content>
+            <ion-button href="/loginService">Log in/Log out</ion-button>
+        </AppCard>
       </div>
-      <div class="back card item-text-wrapped success">
-        <div class="top">
-        </div>
-        <div class="icon-holder">B</div>
-        <div class="answer padding">
-          <p>back side text</p>
-          <p>more back side text</p>
-        </div>
+      <div class="back" bind:this={backChild}>
+        <AppCard colour="orange">
+            <ion-card-header>
+                <ion-card-subtitle>SECONDDDDD Student companion (test) homepage</ion-card-subtitle>
+            </ion-card-header>
+            <ion-button href="/loginservice">Homepage</ion-button>
+            <ion-card-content>
+                Let's list here our quick page links, before we get navigation figured out
+            </ion-card-content>
+            <ion-button href="/loginService">Log in/Log out</ion-button>
+        </AppCard>
       </div>
     </div>
 </div>
@@ -54,49 +77,7 @@
   transform: rotateY(-180deg);
 }
 
-.flip-container,
-.front,
-.back {
-  margin-bottom: 40px;
-  width: 100%;
-  text-align: center;
-}
 
-.flip-container .top,
-.front .top,
-.back .top {
-  padding: 30px 20px;
-}
-
-.flip-container .top,
-.flip-container .icon-holder,
-.front .top,
-.front .icon-holder,
-.back .top,
-.back .icon-holder {
-  background-color: #777777;
-  color: #fff;
-}
-
-.flip-container .icon-holder,
-.front .icon-holder,
-.back .icon-holder {
-  margin: -25px auto 0;
-  border-radius: 50%;
-  border: 6px solid #fff;
-  width: 50px;
-  height: 50px;
-  padding: 7px 10px 10px;
-  font-size: x-large;
-}
-
-
-.back.success .top,
-.back.success .icon-holder {
-  background-color: #23AE89;
-  color: #fff;
-}
-/* flip speed goes here */
 
 .flipper {
   transition: 0.6s;
@@ -119,9 +100,16 @@
   -moz-backface-visibility: hidden;
   -o-backface-visibility: hidden;
   backface-visibility: hidden;
-  position: absolute;
-  top: 0;
-  left: 0;
+}
+
+.front {
+    position: relative;
+}
+
+.back {
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 /* front pane, placed above back */
 
