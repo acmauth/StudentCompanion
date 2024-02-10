8bc6707a080d34213b8cd8bb69808b131d21276e
RogZero <program0zero@gmail.com>
2024-02-10 07:57:36 +0200

    GUI change & Number limits

--- src/routes/degreeGrade/+page.svelte
+++ src/routes/degreeGrade/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { onMount } from "svelte";
     import { universisGet } from "$lib/dataService";
  
@@ -21,6 +21,9 @@
     onMount(async () => {
         // Fetch a list of the current student's courses
         let courses = (await universisGet("students/me/courses?$top=-1")).value;
+
+        let progressBarCourses = document.querySelector('.progress-courses');
+        progressBarCourses.value = 0;
 
         let last_semester = -1
         let last_semester_unpassed_courses_pushed = true
@@ -67,35 +70,72 @@
 
         // calculate pre existing degree grade with a two digit percision
         degree_grade = sum_passed_courses / ectsSum;
-        degree_grade_string = String(degree_grade.toFixed(2));
+
+        if(degree_grade >= 0 && degree_grade <= 10)
+        {
+            degree_grade_string = String(degree_grade.toFixed(2));
+            progressBarCourses.value = degree_grade / 10.0;
+        }
+        else
+        {
+            degree_grade_string = "0.00";
+            progressBarCourses.value = 0;
+        }
+        
     });   
 
     // Whenever there are changes in the unpassed course grades, the average is recalculated
     // taking count of the new grade assumption
 
-    function updateGrades() 
+    function updateGrades(event: CustomEvent<KeyboardEvent>) 
     {
-        let sum_unpassed_semesters = 0
-        let ectsSumAll = ectsSum;
+        let progressBarCourses = document.querySelector('.progress-courses');
 
-        for (const semesterCourses of unpassed_semesters)
+        const target = event.target as HTMLInputElement;
+
+        let sum_unpassed_semesters = 0
+        let ects_sum_all = ectsSum;
+
+        for (const semester_courses of unpassed_semesters)
         {
-            for (const course of semesterCourses.courses)
+            for (const course of semester_courses.courses)
             {
-                if (course.grade > 0 && course.grade != null)
+
+                if (course.grade != null && course.grade != 0)
                 {
                     if (course.grade > 10)
+                    {
+                        target.value = "10";
                         course.grade = 10;
-
-                    ectsSumAll += course.ects;
+                    }
+                    if (course.grade < 0)
+                    {
+                        target.value = "0";
+                        course.grade = 0;
+                        continue;
+                    }
+                    
+                    ects_sum_all += course.ects;
                     sum_unpassed_semesters += course.grade * course.ects;
                 }
 
             }
         }
+        
+        degree_grade = ((sum_unpassed_semesters+sum_passed_courses) / ects_sum_all);
 
-        degree_grade = ((sum_unpassed_semesters+sum_passed_courses) / ectsSumAll);
-        degree_grade_string = String(degree_grade.toFixed(2));
+        if(degree_grade >= 0 && degree_grade <= 10)
+        {
+            degree_grade_string = String(degree_grade.toFixed(2));
+            progressBarCourses.value = degree_grade / 10.0;
+        }
+        else
+        {
+            degree_grade_string = "0.00";
+            progressBarCourses.value = 0;
+        }
+
+        
     }
 </script>
 
@@ -112,27 +152,30 @@
         
         <div class="center-div">
 
-            <ion-card-header class="ion-text-center info">
-                <ion-card-title>Για τον υπολογισμό του βαθμού σας,
-                    πρέπει να δώσετε μια πρόβλεψη για τα μη περασμένα μαθήματά σας [0-10]:</ion-card-title>
-            </ion-card-header>
+            <div style="margin-top: 0.5rem; margin-bottom: 1.5rem">
+                <center>
+                <h2>Για τον υπολογισμό του βαθμού σας,
+                    πρέπει να δώσετε μια πρόβλεψη για τα μη περασμένα μαθήματά σας [0-10]:</h2>
+                </center>
+            </div>
 
             {#if not_passed_all_courses}
             {#each unpassed_semesters as semester}
-            <ion-card class="ion-padding" style="margin-bottom: 20px;">
-            <ion-card-header>Εξάμηνο: {semester.semester}</ion-card-header>
+                <ion-card class="ion-padding" style="margin-bottom: 20px;">
+                <ion-card-header>Εξάμηνο: {semester.semester}</ion-card-header>
 
-            {#each semester.courses as course}
-                <ion-item >
-                    <ion-text class="class">{course.title}</ion-text>
-                    <input  type="number" bind:value={course.grade} min="0" max="10" step="0.1" on:input={updateGrades} >
-                </ion-item>
+                    {#each semester.courses as course}
+                        <!-- Class and input -->
+                        <ion-item >
+                            <ion-text class="class">{course.title}</ion-text>
+                            <input  type="number" pattern="\d*" bind:value={course.grade} min="0" max="10" step="0.1" on:input={updateGrades} placeholder="5.5" />
+                        </ion-item>
+                    {/each}
+
+                </ion-card>
             {/each}
 
-            </ion-card>
-            {/each}
-
-            <!-- Skeleton start -->
+            <!-- Skeleton -->
             {:else}
             <ion-card class="ion-padding" style="margin-bottom: 20px;">
                 <ion-card-header>
@@ -156,14 +199,13 @@
                 </ion-item>
                 
             </ion-card>
-            <!-- Skeleton finish -->
             {/if}  
+            
 
-            <ion-card class="ion-padding">
-                <ion-card-header class="ion-text-center info">
-                    <ion-card-title>Βαθμός πτυχίου: {degree_grade_string}</ion-card-title>
-                </ion-card-header>
-            </ion-card>
+            <div style="margin-top: 3rem;">
+                <center> <h1 style="font-weight: bold; color: #1e2023;">Βαθμός πτυχίου: {degree_grade_string} </h1> </center>
+                    <ion-progress-bar class="progress-courses" />
+            </div>
         </div>
 
       
@@ -188,6 +230,11 @@
     padding: 10px; /* Add padding as needed */
     }
 
+    ion-item
+    {
+        --inner-border-width: 0 0 0 0;
+    }
+
     input[type="number"] {
     appearance: textfield; /* Remove default arrows in some browsers */
     -webkit-appearance: textfield; /* Remove default arrows in Safari */
@@ -200,7 +247,7 @@
     flex: 0.5; /* Takes 1/4 of the available space */
     }
 
-    /* Style the number input when it's being hovered */
+    /* Style the number input when it's being hovered -> Phones don't have hover*/
     input[type="number"]:hover {
     border-color: #888; /* Change border color on hover */
     }
@@ -209,7 +256,25 @@
     input[type="number"]:focus {
     outline: none; /* Remove the default focus outline */
     box-shadow: 0 0 5px #aaa; /* Add a box shadow on focus */
-}
+    }
+
+    .progress-courses {
+		--progress-background: #3880FF;
+		background: #ffffff; /*var(--app-color-primary-dark)*/
+		height: 0.5rem;
+        width: 60%;
+		margin-top: 0.5rem;
+        margin-bottom: 5rem;
+        margin-inline: auto;
+		border-radius: 15px;
+	}
 
 </style>
 
+
+
+<!-- NOTES 
+1. In firefox, unlike chrome and edge (expect 'e'), you can input alphabetical characters
+2. Progress bar sometimes starts full?
+3.
+-->
