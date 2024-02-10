74b59326504f306ce2a1f8542e9b99fd4a7888bf
Neron png <neronmkp@csd.auth.gr>
2024-02-10 23:48:16 +0200

    Fixed navbar topbar colour

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,24 @@
 <script>
 	import { setupIonicBase } from 'ionic-svelte';
-	import { App as capacitorApp } from '@capacitor/app';
+	import { nativeSettings } from './nativeSettings';
+	import { Capacitor } from '@capacitor/core';
+
 	/* Import all components - or do partial loading - see below */
 	import 'ionic-svelte/components/all';
 
 	/* Call Ionic's setup routine */
 	setupIonicBase();
 
-	/* Capacitor back button handling */
-	capacitorApp.addListener('backButton', ({canGoBack}) => {
-		if(!canGoBack){
-			capacitorApp.exitApp();
-		} else {
-			window.history.back();
-		}
-	});
+	/* Check if running on mobile */
+	const isMobile = Capacitor.isNativePlatform() && Capacitor.getPlatform() !== 'web';
+	if (isMobile) {
+		// Apply native settings
+		nativeSettings();
+	}
+
 	/* Theme variables */
 	import '../theme/variables.css';
-
-
-
+	
 </script>
 
 <ion-app>
