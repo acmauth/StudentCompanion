4545a82e23bdb3812012ddc53ed882d0e0b891a7
RogZero <program0zero@gmail.com>
2024-02-16 16:31:36 +0200

    Fixes...

--- src/routes/degreeCalculator/+page.svelte
+++ src/routes/degreeCalculator/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { universisGet } from "$lib/dataService";
- 
+    import { Capacitor } from '@capacitor/core';
 
-    /**
-     * @type {any[]}
-     */
-    let unpassed_semesters = [];
-    let temp_courses = [];
+     
+    let unpassed_semesters: any[] = [];
+    let temp_courses: { title: any; grade: number; ects: any; }[] = [];
 
     let not_passed_all_courses = false; // If false then, the student has passed all of their courses
 
@@ -16,8 +14,35 @@
     let degree_grade = 0;
     let degree_grade_string = "";
 
-    let ectsSum = 0;
+    let ectsSum = 0;    
+    
+    let footer_bottom = 0;
+    
+    // Function to check if the "Degree Grade = x.yz" is in the viewport
+    function isGraphVisible() {
+        // Get references to the graph container and the footer
+        const graphContainer = document.getElementById('gradeGraph');
+        const footer = document.getElementById('footer');
         
+        const gradeRect = graphContainer.getBoundingClientRect();   
+        const footerRect = footer.getBoundingClientRect();
+
+        if(footerRect.bottom != 0)
+         footer_bottom = footerRect.bottom;
+
+        console.log("FOOTER BOTTOM = ", footerRect.bottom, " GRADE TOP =", gradeRect.top);
+        return footer_bottom >= gradeRect.top;
+    }
+
+    // Toggle footer visibility if "Degree Grade = x.yz" is visible
+    function toggleFooterVisibility() {
+        if (isGraphVisible()) 
+            document.getElementById("footer").style.display = "none";
+        else 
+            document.getElementById("footer").style.display = "initial";
+    }   
+
+
     onMount(async () => {
         // Fetch a list of the current student's courses
         let courses = (await universisGet("students/me/courses?$top=-1")).value;
@@ -31,7 +56,7 @@
         // Loop through the courses. If the current course is
         // not passed) change the not_passed_all_courses bool to true and push the courses to the unpassed_semesters list
         // passed) add the ect calculated grade to the sum of the passed courses grade
-        courses.sort((a, b) => a.semester.id - b.semester.id);
+        courses.sort((a: { semester: { id: number; }; }, b: { semester: { id: number; }; }) => a.semester.id - b.semester.id);
         console.log(courses);
 
         for (const course of courses)
@@ -44,7 +69,7 @@
                 {
                     last_semester_unpassed_courses_pushed = true
                     if (last_semester != -1)
-                        unpassed_semesters.push({courses: temp_courses, semester: course.semester.id});
+                        unpassed_semesters.push({courses: temp_courses, semester: course.semester.id}); // Change semester: to semesterId
 
                     temp_courses = [{title: course.courseTitle, grade: 0, ects: course.ects}];
                     last_semester = course.semester.id;
@@ -80,14 +105,14 @@
         {
             degree_grade_string = "0.00";
             progressBarCourses.value = 0;
-        }
-        
+        } 
+
+        setTimeout(() => toggleFooterVisibility(), 10);   
     });   
 
-    // Whenever there are changes in the unpassed course grades, the average is recalculated
-    // taking count of the new grade assumption
+    // Updates the degree grade based on user input
 
-    function updateGrades(event: CustomEvent<KeyboardEvent>) 
+    function handleInpt(event: CustomEvent<KeyboardEvent>) 
     {
         let progressBarCourses = document.querySelector('.progress-courses');
 
@@ -134,87 +159,136 @@
             degree_grade_string = "0.00";
             progressBarCourses.value = 0;
         }
-
-        
     }
 </script>
 
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<ion-footer mode="ios" id="footer">
+	<ion-toolbar mode={Capacitor.getPlatform() != 'ios' ? 'md': undefined}>
+		<ion-title style="text-align: center;">Βαθμός πτυχίου = <span style="color: #0054e9;">{degree_grade_string}</span></ion-title>
+	</ion-toolbar>
+</ion-footer>
 
-<ion-header>
-    <ion-toolbar>
-        <ion-title>Degree Grade</ion-title>
-    </ion-toolbar>
-</ion-header>
 
-<ion-content>
+<div style="overflow:scroll" on:scroll={toggleFooterVisibility}> 
         
-        <div class="center-div">
+    <div class="center-div">
 
-            <div style="margin-top: 0.5rem; margin-bottom: 1.5rem">
-                <center>
-                <h2>Για τον υπολογισμό του βαθμού σας,
-                    πρέπει να δώσετε μια πρόβλεψη για τα μη περασμένα μαθήματά σας [0-10]:</h2>
-                </center>
-            </div>
+        <div style="margin-top: 0.5rem; margin-bottom: 1.5rem">
+            <center>
+            <h3 style="color: #070707;">Για τον υπολογισμό του βαθμού σας,
+                πρέπει να δώσετε μια πρόβλεψη για τα μη περασμένα μαθήματά σας [0-10]:</h3>
+            </center>
+        </div>
 
-            {#if not_passed_all_courses}
+        {#if not_passed_all_courses}
             {#each unpassed_semesters as semester}
                 <ion-card class="ion-padding" style="margin-bottom: 20px;">
-                <ion-card-header>Εξάμηνο: {semester.semester}</ion-card-header>
+                <ion-card-header>{semester.semester}ο Εξάμηνο</ion-card-header > 
 
                     {#each semester.courses as course}
                         <!-- Class and input -->
                         <ion-item >
                             <ion-text class="class">{course.title}</ion-text>
-                            <input  type="number" pattern="\d*" bind:value={course.grade} min="0" max="10" step="0.1" on:input={updateGrades} placeholder="5.5" />
+                            <input type="number" pattern="\d*" bind:value={course.grade} min="0" max="10" step="0.1" on:input={handleInpt} placeholder="5.5"/>
                         </ion-item>
                     {/each}
 
                 </ion-card>
             {/each}
 
-            <!-- Skeleton -->
-            {:else}
-            <ion-card class="ion-padding" style="margin-bottom: 20px;">
-                <ion-card-header>
-                    <ion-skeleton-text animated="true" style="width: 100px"></ion-skeleton-text>
-                </ion-card-header>
+        <!-- Skeleton -->
+        {:else}
+        <ion-card class="ion-padding" style="margin-bottom: 20px;">
+            <ion-card-header>
+                <ion-skeleton-text animated="true" style="width: 100px"></ion-skeleton-text>
+            </ion-card-header>
 
-                <ion-item>
-                    <ion-text class="class">
-                        <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
-                    </ion-text>
-                </ion-item>
-                <ion-item>
-                    <ion-text class="class">
-                        <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
-                    </ion-text>
-                </ion-item>
-                <ion-item>
-                    <ion-text class="class">
-                        <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
-                    </ion-text>
-                </ion-item>
-                
-            </ion-card>
-            {/if}  
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
             
+        </ion-card>
+        {/if}  
 
-            <div style="margin-top: 3rem;">
-                <center> <h1 style="font-weight: bold; color: #1e2023;">Βαθμός πτυχίου: {degree_grade_string} </h1> </center>
-                    <ion-progress-bar class="progress-courses" />
-            </div>
+        {#if not_passed_all_courses}
+        {#each unpassed_semesters as semester}
+            <ion-card class="ion-padding" style="margin-bottom: 20px;">
+            <ion-card-header>{semester.semester}ο Εξάμηνο</ion-card-header>
+
+                {#each semester.courses as course}
+                    <!-- Class and input -->
+                    <ion-item >
+                        <ion-text class="class">{course.title}</ion-text>
+                        <input  type="number" pattern="\d*" bind:value={course.grade} min="0" max="10" step="0.1" on:input={handleInpt} placeholder="5.5" />
+                    </ion-item>
+                {/each}
+
+            </ion-card>
+        {/each}
+
+        <!-- Skeleton -->
+        {:else}
+        <ion-card class="ion-padding" style="margin-bottom: 20px;">
+            <ion-card-header>
+                <ion-skeleton-text animated="true" style="width: 100px"></ion-skeleton-text>
+            </ion-card-header>
+
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
+            <ion-item>
+                <ion-text class="class">
+                    <ion-skeleton-text animated="true" style="width: 200px"></ion-skeleton-text>
+                </ion-text>
+            </ion-item>
+            
+        </ion-card>
+        {/if}  
+        
+
+        <div style="margin-top: 3rem;">
+            <center> <h1 style="font-weight: bold; color: #1e2023;" id="gradeGraph">Βαθμός πτυχίου: {degree_grade_string} </h1> </center>
+                <ion-progress-bar class="progress-courses"/>
         </div>
+    </div>
 
-      
-</ion-content>
+    
+</div>
     <!-- Check if there are any unpassed courses and if so, show them in the screen and ask for input -->
 
 
 
 <style>  
+    ion-toolbar {
+        --border-color: #ffffff;
+
+        --min-height: 1rem;
+        --padding-top: 10px;
+        --padding-bottom: 10px;
+    }
+
+
+
     .center-div {
         width: auto;
         @media (min-width: 769px) {
@@ -274,7 +348,8 @@
 
 
 <!-- NOTES 
-1. In firefox, unlike chrome and edge (expect 'e'), you can input alphabetical characters
-2. Progress bar sometimes starts full?
-3.
+0. Footer visibility initialization -> done
+1. In firefox, unlike chrome and edge (expect 'e'), you can input alphabetical characters ...
+2. Progress bar sometimes starts full? -> Fixed?
+3. Change semester: to semesterId -> Tidy everything ...
 -->
