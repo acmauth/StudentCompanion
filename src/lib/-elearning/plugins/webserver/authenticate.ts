7a3afd8abb1721a136cda016c585b1827f450d92
Neron png <neronmkp@csd.auth.gr>
2024-02-16 16:45:44 +0200

    Rebased and fixed a small bug in auth

--- src/lib/-elearning/plugins/webserver/authenticate.ts
+++ src/lib/-elearning/plugins/webserver/authenticate.ts
@@ -1,7 +1,9 @@
 import type { AuthenticationResult } from "$lib/-elearning/types";
 
 export async function authenticate(username: string, password: string): Promise<AuthenticationResult> {
-    const tokenGrab = await fetch("/_elearningService?username="+username+"&password="+password, {
+    let encodedUsername = encodeURIComponent(username);
+    let encodedPassword = encodeURIComponent(password);
+    const tokenGrab = await fetch("/_elearningService?username="+encodedUsername+"&password="+encodedPassword, {
         method: "GET"
     });
     const response = await tokenGrab.json();
