e208356cd9e6362c1885d1a74c2e8db937c79d75
Konstantinos Agathopoulos <kostaga203@gmail.com>
2024-02-17 14:55:11 +0200

    added settings

--- src/lib/components/personalInfo/infoItem.svelte
+++ src/lib/components/personalInfo/infoItem.svelte
@@ -2,7 +2,8 @@
 	import man from '$lib/assets/man.png';
 	import woman from '$lib/assets/woman.png';
 	import * as allIonicIcons from 'ionicons/icons';
-	import '../../../theme/variables.css';
+	import Settings from "./settings.svelte";
+
 	/**
 	 * @type {string}
 	 */
@@ -49,12 +50,9 @@
 	 /**
 	 * @type {any}
 	 */
-	  export let logOut;
 </script>
 
 
-
-<ion-content>
 	<ion-card class="ion-padding">
 		<ion-card-header class="ion-text-center info">
 			{#if gender === 'Α'}
@@ -127,16 +125,9 @@
 			</ion-item>
 			{/if}
 
-			<ion-item lines="none" on:click={logOut}>
-				<ion-icon color="danger" size="small" icon={allIonicIcons.exit} />
 
-				<ion-label color="danger" class="ion-padding-start">Αποσύνδεση</ion-label>
-			</ion-item>
-
-
-		</ion-card-content>
 	</ion-card>
-</ion-content>
+	
 
 
 
