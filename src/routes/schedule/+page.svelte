2a617e1f268eb3d9dc0e678b326db25fddf482d1
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2024-02-21 14:33:50 +0200

    Redesigned day selection

--- src/routes/schedule/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { goto } from "$app/navigation";
-	import { getDayByIndex } from "$components/schedule/day/days";
-	import { onMount } from "svelte";
-
-    onMount(() => {
-        goto("/schedule/classes/" + getDayByIndex(new Date().getDay()).toLowerCase());
-    });
-</script>
