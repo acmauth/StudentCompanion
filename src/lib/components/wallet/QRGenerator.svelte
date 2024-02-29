86b4e1d6bdd1aaf6afd93a2895f13cc564901e96
balaktsisc <mpalaktsc@csd.auth.gr>
2024-02-29 02:54:41 +0200

    Resolved conflicts

--- src/lib/components/wallet/QRGenerator.svelte
+++ src/lib/components/wallet/QRGenerator.svelte
@@ -1,19 +1,60 @@
 <script lang='ts'>
   import { onMount } from 'svelte';
-  // import QRCode from 'qrcode-generator';
   import QRCode from 'qrcode'
 	import type { qrItem } from './qrItem';
-  
+  import { alertController } from 'ionic-svelte';
+  import { qrStore } from './qrStore';
+	import { goto } from '$app/navigation';
+	import { reload } from 'ionicons/icons';
+
   export let qr1: qrItem;
 
   onMount(() => {
     const qrCodeElement = document.getElementById(qr1.data);
     QRCode.toCanvas(qrCodeElement, qr1.data, { width: 200, height: 200 });
   });
+
+  const showAlert = async (options) => {
+    const alert = await alertController.create(options);
+    await alert.present();
+  };
+
+  function deleteQR() {
+    const options = {
+      header: 'Αφαίρεση QR;',
+      message: 'Θέλεις να αφαιρεθεί το QR από το πορτοφόλι σου;',
+      buttons: [{
+            text: 'ΟΧΙ',
+            role: 'cancel',
+            handler: () => {
+               return;
+            }
+         },
+         {
+            text: 'ΝΑΙ',
+            role: 'confirm',
+            handler: () => {
+              qrStore.update((store) => store.filter((item) => item.data !== qr1.data));
+            }
+         }]
+    };
+    showAlert(options);
+  }
 </script>
 
 
-<div style="display: flex; flex-direction: column; align-items: center;">
-  <ion-label>{qr1.title}</ion-label>
-  <canvas style="padding:0; margin:0;" id="{qr1.data}"></canvas>
-</div>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<ion-div on:click={deleteQR}>
+    <canvas id="{qr1.data}"></canvas>
+    <ion-label>{qr1.title}</ion-label>     
+</ion-div>
+
+<style>
+  ion-div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
