b4dd1258c79df3392546faf87c21d0e058ba15c1
Myrto Gk <myrtogk12@gmail.com>
2024-03-11 00:29:25 +0200

    Colorful pins with images-maps

--- src/lib/components/map/map.svelte
+++ src/lib/components/map/map.svelte
@@ -2,9 +2,13 @@
     import { onMount, onDestroy } from 'svelte';
     import { browser } from '$app/environment';
     import SubPageHeader from '$shared/subPageHeader.svelte';
-    import red_pointer from '$lib/assets/red_pointer.svg'
-    import blue_pointer from '$lib/assets/blue_pointer.svg'
-    import coordinates from "$lib/components/map/coordinates.json"
+    import gym from '$lib/assets/gym.png';
+    import university from '$lib/assets/university.png';
+    import library from '$lib/assets/library.png';
+    import lesxi from '$lib/assets/lesxi.png';
+    import ceremony from '$lib/assets/ceremony.png';
+    import observatory from '$lib/assets/observatory.png';
+    import coordinates from "$lib/components/map/coordinates.json";
 
     const points = coordinates;
 
@@ -21,32 +25,47 @@
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            const redIcon = leaflet.icon({
-                iconUrl:  red_pointer,
-                iconSize: [38, 38],
-                iconAnchor: [19, 38],
-                popupAnchor: [0, -38] 
-            });
-
-            const blueIcon = leaflet.icon({
-                iconUrl:  blue_pointer,
-                iconSize: [38, 38],
-                iconAnchor: [19, 38],
-                popupAnchor: [0, -38] 
-            });
-
             for (const point of points) {
-                const { name, name_el, coordinates, pointer } = point;
-                if (pointer == "blue"){
-                    leaflet.marker([coordinates.lat, coordinates.lng], { icon: blueIcon }).addTo(map)
-                        .bindPopup(name_el);
+
+                const { name, name_el, coordinates, pointer, url } = point;
+                let popupContent = `${name_el}<br><a href=${url}> ${url} </a>`;
+
+                let iconUrl;
+
+                console.log("Pointer ", pointer)
+
+                if (pointer == "department"){
+                    iconUrl = university;
                 }
-                else {
-                    leaflet.marker([coordinates.lat, coordinates.lng], { icon: redIcon }).addTo(map)
-                        .bindPopup(name_el);
+                else if (pointer == "gym"){
+                    iconUrl = gym;
                 }
-                    
+                else if (pointer == "lesxi"){
+                    iconUrl = lesxi;
+                }
+                else if (pointer == "library") {
+                    iconUrl = library;
+                }
+                else if (pointer == "observatory") {
+                    iconUrl = observatory;
+                }
+                else if (pointer == "ceremony") {
+                    iconUrl = ceremony;
+                }
+
+                console.log("Here ", iconUrl)
+
+                const customIcon = leaflet.icon({
+                    iconUrl:  iconUrl,
+                    iconSize: [38, 38],
+                    iconAnchor: [19, 38],
+                    popupAnchor: [0, -38] 
+                });
+
+                console.log(customIcon)
                 
+                const marker = leaflet.marker([coordinates.lat, coordinates.lng], { icon: customIcon }).addTo(map);
+                marker.bindPopup(popupContent);
             }
         }
     });
