2bb310d54f368ad25bff7dd2e4c2c90589f6bed4
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2024-04-10 16:57:39 +0300

    Done some progress

--- src/lib/-webmail/plugins/webserver/dataservice.ts
+++ src/lib/-webmail/plugins/webserver/dataservice.ts
@@ -1,34 +1,18 @@
-import { connect } from 'imap-simple';
+// import { connect } from 'imap-simple';
 import { userCreds } from "$stores/credentials.store";
 import { get } from "svelte/store";
 import type { WebmailInboxRequestResponse } from "$lib/-webmail/types.ts";
 
 export async function getInbox(): Promise<WebmailInboxRequestResponse> {
-  const config = {
-    imap: {
-      user: get(userCreds).username,
-      password: get(userCreds).password,
-      host: 'mail.auth.gr',
-      port: 993,
-      tls: true,
-    },
-  };
-  
-  try {
-    const connection = await connect(config);
-    await connection.openBox('INBOX');
-    const searchCriteria = ['ALL'];
-    const fetchOptions = {
-      bodies: [''],
-      struct: true,
-      markSeen: false,
-      reverse: true,
-      limit: 8,
-    };
-    const emails = await connection.search(searchCriteria, fetchOptions);
-    connection.end();
-    return { error: null, received: emails.map(email => email.parts[0].body) };
-  } catch (error) {
-    throw error;
-  }
+    const inboxData = await fetch("/_webmailService", {
+        method: "POST",
+        body: JSON.stringify({  username : get(userCreds).username,
+                                password : get(userCreds).password,
+                                server : 'mail.auth.gr',
+                                port : 993,
+                                count : 8
+                              })
+    });
+    
+    return (await inboxData.json()) as WebmailInboxRequestResponse;
 }
--- src/routes/_webmailService/+server.ts
+++ src/routes/_webmailService/+server.ts
@@ -1,7 +1,41 @@
-import { webmailInboxRequest } from '../../lib/-webmail/dataService/core';
 import type { RequestHandler } from '@sveltejs/kit';
+import { ImapFlow } from 'imapflow';
 
-export const GET: RequestHandler = async () => {
-	return new Response(JSON.stringify(webmailInboxRequest()));
+export const POST: RequestHandler = async ({ request }) => {
+	
+	const requestBody = await request.json();
+	let response; 
+	let emails = [];
+
+	const client = new ImapFlow({
+		host: requestBody.server,
+		port: requestBody.port,
+		secure: true,
+		auth: {
+			user: requestBody.username,
+			pass: requestBody.password
+		},
+		logger: false
+	});
+
+	await client.connect();
+
+	let lock = await client.getMailboxLock('INBOX');
+	try {
+		let message = await client.fetchOne(client.mailbox.exists, { source: true });
+        console.log(message.source.toString());
+
+        for await (let message of client.fetch('1:2', { envelope: true })) {
+            // console.log(`${message.envelope.subject}`);
+			emails.push(message);
+        }
+		response = { error: null, received: emails };
+	} catch (err) {
+		response = { error: err.message };
+	} finally {
+		lock.release();
+	}
+	
+	await client.logout();
+	return new Response(JSON.stringify(response));
 };
-
