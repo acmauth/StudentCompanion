552c243f68d26bb2b0846860002f656490bf3525
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2024-04-25 12:30:56 +0300

    no idea what im doing

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,24 +1,128 @@
-<script>
-    import CapacitorPersistedStore from '$lib/storage/capacitorPersistedStore';
-    import { onMount } from 'svelte';
-    import { home } from 'ionicons/icons';
+<script lang="ts">
+	import { onMount } from 'svelte';
+  import { register } from 'swiper/element/bundle';
+  import { getDayByIndex } from '$lib/components/schedule/day/days';
+  register();
 
-    let myStore = new CapacitorPersistedStore("testValue", 'myStore');
-    let myOtherStore = new CapacitorPersistedStore("testValue", 'myOtherStore');
+  let openDates: Date[] = [];
 
-    onMount( async () => {
-        await myStore.loadFromStorage();
-        await myOtherStore.loadFromStorage();
+  function generateCalendarDates(year: number, month: number, day: number) {
+    const firstDate = new Date(year, month, day - 3);
+    const lastDate = new Date(year, month, day + 3);
+    const i = new Date(firstDate);
+
+    $: openDates = [];
+    while (i <= lastDate) {
+      $: openDates.push(new Date(i));
+      i.setDate(i.getDate() + 1);
+    }
+  }
+
+  onMount(() => {
+    const today = new Date();
+    generateCalendarDates(today.getFullYear(), today.getMonth(), today.getDay());
+
+    const swiperEl = document.querySelector('swiper-container');
+    
+    swiperEl.addEventListener('swiperslidechangetransitionend', (event) => {
+      let offset: number = 1;
+      if(swiperEl.swiper.swipeDirection == "next")
+        offset = -1;
+      generateCalendarDates(openDates[offset].getFullYear(), openDates[+ offset].getMonth(), openDates[3 + offset].getDay());
     });
+
+  });
+  
 
 </script>
 
-<ion-content>
+<ion-content>  
+<swiper-container on:swipe{handleSwipe} slides-per-view="5" speed="500" mousewheel-force-to-axis="true" loop="false">
+  {#each openDates as date}
+    <swiper-slide id="day-{date.getDay()}">
+      <ion-card>
+          <ion-card-header>
+              <ion-card-title>{getDayByIndex(date.getDay(), 'el', true, 3)}</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <p>{date.getDate()}</p>
+          </ion-card-content>
+      </ion-card>
+    </swiper-slide>
+    {/each}
+  <!-- <swiper-slide id="day-0">
     <ion-card>
         <ion-card-header>
-            <ion-card-title>Δοκιμή</ion-card-title>
+            <ion-card-title>Κυρ</ion-card-title>
         </ion-card-header>
         <ion-card-content>
         </ion-card-content>
     </ion-card>
+</swiper-slide>  
+  <swiper-slide id="day-1">
+      <ion-card>
+          <ion-card-header>
+              <ion-card-title>Δευ</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+          </ion-card-content>
+      </ion-card>
+  </swiper-slide>
+  <swiper-slide id="day-2">
+      <ion-card>
+          <ion-card-header>
+              <ion-card-title>Τρι</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+          </ion-card-content>
+      </ion-card>
+  </swiper-slide>
+  <swiper-slide id="day-3">
+    <ion-card>
+        <ion-card-header>
+            <ion-card-title>Τετ</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+        </ion-card-content>
+    </ion-card>
+</swiper-slide>
+<swiper-slide id="day-4">
+  <ion-card>
+    <ion-card-header>
+        <ion-card-title>Πεμ</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+    </ion-card-content>
+</ion-card>
+</swiper-slide>
+<swiper-slide id="day-5">
+<ion-card>
+  <ion-card-header>
+      <ion-card-title>Παρ</ion-card-title>
+  </ion-card-header>
+  <ion-card-content>
+  </ion-card-content>
+</ion-card>
+</swiper-slide>
+<swiper-slide id="day-6">
+  <ion-card>
+      <ion-card-header>
+          <ion-card-title>Σαβ</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>
+      </ion-card-content>
+  </ion-card>
+</swiper-slide> -->
+</swiper-container>
 </ion-content>
+
+<style>
+  ion-card-title {
+    font-size: 1rem;
+    align-self: center;
+  }
+  ion-card * {
+    padding: 5px;
+    margin-inline: 4px;
+  }
+</style>
