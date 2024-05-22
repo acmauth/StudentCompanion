85928aac67448fb60cdc550daf0a65db1c100348
VasilisMicha <vasilismich789@gmail.com>
2024-05-22 19:41:06 +0300

    started calendar notifications

---
+++ src/lib/scheduledNotifications/calendarNotifications.ts
@@ -0,0 +1,5 @@
+import type { EventTimeSlot, Event, EventRepeatType } from "$components/calendar/event/Event";
+
+async function setNotif(notifyTime?: number, : Date, repeat: EventRepeatType, repeatInterval?: number, repeatUntil: Date ){
+    
+}
