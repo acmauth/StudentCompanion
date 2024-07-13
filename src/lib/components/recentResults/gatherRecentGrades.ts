3cf2a5208b155f9e54d61f723e43b211d7363a8b
VasilisMicha <vasilismich789@gmail.com>
2024-07-13 21:17:50 +0300

    changed some comments in gatherRecentGrades

--- src/lib/components/recentResults/gatherRecentGrades.ts
+++ src/lib/components/recentResults/gatherRecentGrades.ts
@@ -2,14 +2,15 @@ import { neoUniversisGet } from "$lib/dataService";
 
 export async function gatherRecentGrades(refresh: boolean = false){
     const options = {forceFresh: refresh, lifetime: 60 * 60 * 1}
-    // getting the current period and year 
+    // calling the exam periods and years for all the graded subjects in descending order
     let examPeriods = (await neoUniversisGet("students/me/grades?$select=courseExam/year as gradeYear,courseExam/examPeriod as examPeriod&$orderByDescending=courseExam/year&$thenByDescending=courseExam/examPeriod&$top=-1&$count=false", options)).value;
     
     // exam periods: winter=1, spring=3, september=5
+    // last subject has the latest year and period becauses they are sorted
     const currentYear = examPeriods[examPeriods.length-1].gradeYear;
     const currentPeriod = examPeriods[examPeriods.length-1].examPeriod;
     
-    // getting recent grades based on the current period, if empty that exam period didn't arrive
+    // getting recent grades based on the current exam period
     let recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + currentYear + ' and courseExam/examPeriod eq ' + currentPeriod + '&$expand=course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
     
     return recentGrades;
