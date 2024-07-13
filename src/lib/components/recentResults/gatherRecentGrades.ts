5c7482f5c7a94de229ebfd4cf0186a1e1231d117
VasilisMicha <vasilismich789@gmail.com>
2024-07-13 20:53:48 +0300

    removed redundant code

--- src/lib/components/recentResults/gatherRecentGrades.ts
+++ src/lib/components/recentResults/gatherRecentGrades.ts
@@ -10,32 +10,8 @@ export async function gatherRecentGrades(refresh: boolean = false){
     const currentPeriod = examPeriods[examPeriods.length-1].examPeriod;
     
     // getting recent grades based on the current period, if empty that exam period didn't arrive
-    let recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + currentYear + ' and courseExam/examPeriod eq ' + currentPeriod + '&$expand=status,course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
-
-    // if its empty it means that the recent grades are a period prior
-    if (recentGrades.length === 0){
-        let lastPeriod;
-        let lastYear;
-
-        // switch to the previous exam period
-        switch (currentPeriod){
-            case 1:
-                lastPeriod = 5;
-                lastYear = currentYear - 1;
-                break;
-            case 3:
-                lastPeriod = 1;
-                lastYear = currentYear
-                break;
-            case 5:
-                lastPeriod = 3;
-                lastYear = currentYear;
-                break;
-        }
-
-        // getting recent grades from the previous period
-        recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + lastYear + ' and courseExam/examPeriod eq ' + lastPeriod + '&$expand=status,course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
-    }
-
+    let recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + currentYear + ' and courseExam/examPeriod eq ' + currentPeriod + '&$expand=course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
+    console.log(recentGrades);
+    
     return recentGrades;
 }
