46fd2dbd801ba3907875f79daedcafa339fb66a6
VasilisMicha <vasilismich789@gmail.com>
2024-07-13 23:23:26 +0300

    added if statement to check if examPeriods is empty

--- src/lib/components/recentResults/gatherRecentGrades.ts
+++ src/lib/components/recentResults/gatherRecentGrades.ts
@@ -5,13 +5,16 @@ export async function gatherRecentGrades(refresh: boolean = false){
     // calling the exam periods and years for all the graded subjects in descending order
     let examPeriods = (await neoUniversisGet("students/me/grades?$select=courseExam/year as gradeYear,courseExam/examPeriod as examPeriod&$orderByDescending=courseExam/year&$thenByDescending=courseExam/examPeriod&$top=-1&$count=false", options)).value;
     
+    let recentGrades = [];
     // exam periods: winter=1, spring=3, september=5
     // last subject has the latest year and period becauses they are sorted
-    const currentYear = examPeriods[examPeriods.length-1].gradeYear;
-    const currentPeriod = examPeriods[examPeriods.length-1].examPeriod;
+    if (examPeriods.length !==0) {
+        const currentYear = examPeriods[examPeriods.length-1].gradeYear;
+        const currentPeriod = examPeriods[examPeriods.length-1].examPeriod;
     
-    // getting recent grades based on the current exam period
-    let recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + currentYear + ' and courseExam/examPeriod eq ' + currentPeriod + '&$expand=course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
+        // getting recent grades based on the current exam period
+        recentGrades = (await neoUniversisGet('students/me/grades?$filter=courseExam/year eq ' + currentYear + ' and courseExam/examPeriod eq ' + currentPeriod + '&$expand=course($expand=gradeScale,locale),courseClass($expand=instructors($expand=instructor($select=InstructorSummary))),courseExam($expand=examPeriod,year)&$top=-1&$count=false', options)).value;
+    }
     
     return recentGrades;
 }
