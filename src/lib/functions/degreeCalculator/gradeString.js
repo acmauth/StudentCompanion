111bd7ab4a3daf8ccf3b2a16ea3be964383d0196
Konstantinos Agathopoulos <kostaga203@gmail.com>
2024-07-17 14:33:11 +0300

    also fixed degree calculator

--- src/lib/functions/degreeCalculator/gradeString.js
+++ src/lib/functions/degreeCalculator/gradeString.js
@@ -6,7 +6,7 @@ export function gradeString(number) {
     var string = "0.00";
 
     if(number >= 0 && number <= 10)
-        string = String(number.toFixed(2));
+        string = String(Math.floor(number * 100) / 100);
 
     return string;
 };
