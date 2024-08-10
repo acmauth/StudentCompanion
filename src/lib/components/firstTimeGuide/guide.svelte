b6fc7f420a16912567ef49fb2f11c34cff676285
Apostolos Chalis <tolishalis@gmail.com>
2024-08-10 13:18:24 +0300

    Modal guide placeholder

--- src/lib/components/firstTimeGuide/guide.svelte
+++ src/lib/components/firstTimeGuide/guide.svelte
@@ -1,1 +1,34 @@
-<h1>Guide</h1> 
+<script>
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  let modalElement;
+
+  onMount(() => {
+    modalElement.present();
+  });
+
+  function dismiss() {
+    modalElement.dismiss();
+    goto("/pages/homepage");
+  }
+</script>
+
+<ion-modal bind:this={modalElement}>
+  <div class="modal-content">
+    <h1>Guide Placeholder</h1>
+    <ion-button on:click={dismiss}>Close</ion-button>
+  </div>
+</ion-modal>
+
+<style>
+  .modal-content {
+    padding: 16px;
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 16px;
+  }
+</style>
+
