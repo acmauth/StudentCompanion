2c2470266f73bd35317822e2159481dec4f150d6
Konstantinos Agathopoulos <kostaga203@gmail.com>
2024-10-02 18:12:04 +0300

    testing versions

--- src/lib/globalFunctions/checkVersion.ts
+++ src/lib/globalFunctions/checkVersion.ts
@@ -1,24 +1,6 @@
 import { Capacitor } from '@capacitor/core';
-import { AppUpdate, AppUpdateInfo, AppUpdateAvailability } from '@capawesome/capacitor-app-update';
+import { AppUpdate, AppUpdateAvailability } from '@capawesome/capacitor-app-update';
 import { alertController } from '@ionic/core';
-
-const getCurrentAppVersion = async () => {
-	const result = await AppUpdate.getAppUpdateInfo();
-	if (Capacitor.getPlatform() === 'android') {
-		return result.currentVersionCode;
-	} else {
-		return result.currentVersionName;
-	}
-};
-
-const getAvailableAppVersion = async () => {
-	const result = await AppUpdate.getAppUpdateInfo();
-	if (Capacitor.getPlatform() === 'android') {
-		return result.availableVersionCode;
-	} else {
-		return result.availableVersionName;
-	}
-};
 
 const openAppStore = async () => {
 	await AppUpdate.openAppStore();
@@ -44,49 +26,71 @@ const startFlexibleUpdate = async () => {
 	}
 };
 
-const completeFlexibleUpdate = async () => {
-	await AppUpdate.completeFlexibleUpdate();
-};
-
 const checkForUpdates = async () => {
 	try {
-		const result = await AppUpdate.getAppUpdateInfo();
-		handleUpdate(result);
+		fetch(
+			'https://raw.githubusercontent.com/acmauth/StudentCompanion/refs/heads/appVersion/version.json'
+		).then(async (response) => {
+			const data = await response.json();
+			handleUpdate(data);
+		});
 	} catch (error) {
 		console.error('Error fetching update info: ', error);
 	}
 };
 
-const handleUpdate = async (result: AppUpdateInfo) => {
-	console.log('kek2 ' + JSON.stringify(result));
-	if (result.updateAvailability === AppUpdateAvailability.UPDATE_AVAILABLE) {
-		let message = '';
+const handleUpdate = async (result: any) => {
+	const platform = Capacitor.getPlatform() === 'android' ? 'android' : 'ios';
+	const priority = result[platform].priority;
+	const update = await AppUpdate.getAppUpdateInfo();
 
-		message = 'A moderate update is available. Please update the app for a better experience.';
-
-		const alert = await alertController.create({
-			header: 'Update Available',
-			message: message,
-			buttons: [
-				{
-					text: 'Update Now',
-					handler: async () => {
-						if (result.immediateUpdateAllowed) {
-							await performImmediateUpdate();
-						} else if (result.flexibleUpdateAllowed) {
-							await startFlexibleUpdate();
-						} else {
-							await openAppStore();
+	if (update.updateAvailability === AppUpdateAvailability.UPDATE_AVAILABLE) {
+		if (priority === 5) {
+			const alert = await alertController.create({
+				header: 'Update Required',
+				message: 'A new version of the app is available. Please update to the latest version.',
+				buttons: [
+					{
+						text: 'Update Now',
+						handler: async () => {
+							if (result.immediateUpdateAllowed) {
+								await performImmediateUpdate();
+							} else if (result.flexibleUpdateAllowed) {
+								await startFlexibleUpdate();
+							} else {
+								await openAppStore();
+							}
 						}
 					}
-				},
-				{
-					text: 'Later',
-					role: 'cancel'
-				}
-			]
-		});
-		await alert.present();
+				]
+			});
+
+			await alert.present();
+		} else if (priority >= 2 && priority < 5) {
+			const alert = await alertController.create({
+				header: 'Update Available',
+				message: 'A new version of the app is available. Would you like to update?',
+				buttons: [
+					{
+						text: 'Update Now',
+						handler: async () => {
+							if (result.immediateUpdateAllowed) {
+								await performImmediateUpdate();
+							} else if (result.flexibleUpdateAllowed) {
+								await startFlexibleUpdate();
+							} else {
+								await openAppStore();
+							}
+						}
+					},
+					{
+						text: 'Later',
+						role: 'cancel'
+					}
+				]
+			});
+			await alert.present();
+		}
 	}
 };
 
