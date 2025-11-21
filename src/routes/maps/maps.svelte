a61ea31013e7cc914c129b2bde5ab9215cf5b4ac
Neron Panagiotopoulos <neronmkp@csd.auth.gr>
2025-11-21 12:42:32 +0200

    Tempfix for metro

--- src/routes/maps/maps.svelte
+++ src/routes/maps/maps.svelte
@@ -120,7 +120,16 @@
 			renderMarkers();
 
 			// Fetch metro information
+			// Check if current date is between 10 November 2025 and 10 December 2025
+			const currentDate = new Date();
+			const startDate = new Date('2025-11-10');
+			const endDate = new Date('2025-12-10');
+
+			if (currentDate >= startDate && currentDate <= endDate) {
+				metroInfo = 'Προσορινή διακοπή επιβατικών διαδρομών λόγω δοκιμών επέκτασης Καλαμαριάς μεταξύ 10 Νοεμβρίου και 10 Δεκεμβρίου';
+			} else {
 			metroInfo = await getMetroInfo();
+			}
 		}
 	});
 
