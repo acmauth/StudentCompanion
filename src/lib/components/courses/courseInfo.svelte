fb16dfc6631af17dd03b237f8f53719f3f997e09
Dimitra Angelidou <dangelidou@csd.auth.gr>
2026-01-11 19:06:06 +0200

    several changes and deletions of more deprecated code

--- src/lib/components/courses/courseInfo.svelte
+++ src/lib/components/courses/courseInfo.svelte
@@ -1,8 +1,7 @@
-<script>
+<script lang="ts">
 	import { afterUpdate, onMount } from 'svelte';
 	import Chart from 'chart.js/auto';
-	import { t } from "$lib/i18n";
-
+	import { t } from '$lib/i18n';
 
 	/**
 	 * @type {{ title: any; code: any; courseType: any; teacher: string; ects: any; semester: any; season: any; weeklyHours: any; period: any; }}
@@ -20,17 +19,19 @@
 	/**
 	 * @type {HTMLElement | null}
 	 */
-	let numberElement;
-	let gradeData = [];
+	let numberElement: HTMLElement | null;
+	let gradeData: number[] = [];
 	let grades = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	let maximum = 0;
 	let chart;
 
-	const primaryColor = getComputedStyle(document.body).getPropertyValue('--app-color-stat-graph').trim();
+	const primaryColor = getComputedStyle(document.body)
+		.getPropertyValue('--app-color-stat-graph')
+		.trim();
 
 	// Slot machine animation function
 	function animate() {
-		const randomNumber = currentIteration+1;
+		const randomNumber = currentIteration + 1;
 		if (numberElement) {
 			numberElement.textContent = randomNumber.toString();
 			if (randomNumber < maxIterations) {
@@ -66,7 +67,10 @@
 			maximum = Math.max(...gradeData);
 
 			// Create a bar chart using Chart.js
-			const ctx = document.getElementById('statChart').getContext('2d');
+			const canvas = document.getElementById('statChart') as HTMLCanvasElement;
+			if (!canvas) return;
+			const ctx = canvas.getContext('2d');
+			if (!ctx) return;
 			const colors = grades.map((grade) => (grade < 5 ? '#515151' : primaryColor));
 			chart = new Chart(ctx, {
 				type: 'bar',
@@ -96,14 +100,13 @@
 
 						title: {
 							display: true,
-							text: $t("course.dist"),
+							text: $t('course.dist'),
 							font: {
-								size: 15,
+								size: 15
 							},
 							color: primaryColor
 						}
 					},
-
 
 					layout: {
 						padding: {
@@ -119,155 +122,142 @@
 			console.log('Πρόβλημα εμφάνισης', error);
 		}
 	});
-
-
-	
 </script>
 
+<ion-card>
+	<ion-card-header>
+		<ion-card-title class="title">{course.title}</ion-card-title>
+		<ion-card-subtitle color="primary"># {course.code}</ion-card-subtitle>
+		<ion-card-subtitle>{$t('course.type')} {course.courseType}</ion-card-subtitle>
+	</ion-card-header>
 
-	<ion-card>
-		<ion-card-header>
-			<ion-card-title class="title">{course.title}</ion-card-title>
-			<ion-card-subtitle color="primary"># {course.code}</ion-card-subtitle>
-			<ion-card-subtitle>{$t("course.type")} {course.courseType}</ion-card-subtitle>
-		</ion-card-header>
+	<!-- Checking if there is a grade and displays the corresponding color depending if you passed or not -->
+	<ion-card-content class="ion-text-center">
+		{#if !stats.grade && stats.grade != 0}
+			<ion-text><b>-</b></ion-text>
+			<br />
+			<ion-text class="danger">{$t('course.graded')}</ion-text>
+		{:else if stats.grade >= 5}
+			<ion-text class="success" id="number">{stats.grade}</ion-text>
+		{:else}
+			<ion-text class="danger" id="number">{stats.grade}</ion-text>
+		{/if}
 
-		<!-- Checking if there is a grade and displays the corresponding color depending if you passed or not -->
-		<ion-card-content class="ion-text-center">
-			{#if !stats.grade && stats.grade!=0}
-				<ion-text><b>-</b></ion-text>
-				<br>
-				<ion-text class="danger">{$t("course.graded")}</ion-text>
-			{:else if stats.grade >= 5}
-				<ion-text class="success" id="number">{stats.grade}</ion-text>
-			{:else}
-				<ion-text class="danger" id="number">{stats.grade}</ion-text>
-			{/if}
+		<!-- Course info -->
 
-			<!-- Course info -->
+		<ion-list>
+			<ion-item lines="full">
+				<ion-label>ECTS</ion-label>
+				<ion-text slot="end">{course.ects}</ion-text>
+			</ion-item>
 
-			<ion-list>
-
-				<ion-item lines ="full">
-					<ion-label>ECTS</ion-label>
-					<ion-text slot="end">{course.ects}</ion-text>
-				</ion-item>
-
-				<ion-item lines ="full">
-					<ion-label>{$t("course.semester")}</ion-label>
-					{#if course.semester <= 24}
+			<ion-item lines="full">
+				<ion-label>{$t('course.semester')}</ion-label>
+				{#if course.semester <= 24}
 					<ion-text slot="end">{course.semester}ο</ion-text>
-					{:else}
+				{:else}
 					<ion-text slot="end">{course.season}</ion-text>
-					{/if}
-				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.period")}</ion-label>
-					<ion-text slot="end">{course.season}</ion-text>
-				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.weekly_hours")}</ion-label>
-					<ion-text slot="end">{course.weeklyHours}</ion-text>
-				</ion-item>
+				{/if}
+			</ion-item>
+			<ion-item lines="full">
+				<ion-label>{$t('course.period')}</ion-label>
+				<ion-text slot="end">{course.season}</ion-text>
+			</ion-item>
+			<ion-item lines="full">
+				<ion-label>{$t('course.weekly_hours')}</ion-label>
+				<ion-text slot="end">{course.weeklyHours}</ion-text>
+			</ion-item>
 
-				<ion-accordion-group class="accordion" expand="compact">
-					<ion-accordion value="first">
-						<ion-item slot="header" color="white">
-							<ion-label>{$t("course.professors")}</ion-label>
-						</ion-item>
-						{#each course.teacher.split(', ') as teacher}
-							<h3 class="teachers" slot="content">{teacher}</h3>
-						{/each}
-					</ion-accordion>
-				</ion-accordion-group>
-			</ion-list>
-		</ion-card-content>
-	</ion-card>
+			<ion-accordion-group class="accordion" expand="compact">
+				<ion-accordion value="first">
+					<ion-item slot="header" color="white">
+						<ion-label>{$t('course.professors')}</ion-label>
+					</ion-item>
+					{#each course.teacher.split(', ') as teacher}
+						<h3 class="teachers" slot="content">{teacher}</h3>
+					{/each}
+				</ion-accordion>
+			</ion-accordion-group>
+		</ion-list>
+	</ion-card-content>
+</ion-card>
 
-	<ion-card>
-		<ion-card-header>
-			<ion-card-title class="title">{$t("course.stats")}</ion-card-title>
-			{#if course.period}
-			<ion-card-subtitle>{$t("course.exams")} {course.period}</ion-card-subtitle>
-			{:else}
+<ion-card>
+	<ion-card-header>
+		<ion-card-title class="title">{$t('course.stats')}</ion-card-title>
+		{#if course.period}
+			<ion-card-subtitle>{$t('course.exams')} {course.period}</ion-card-subtitle>
+		{:else}
 			<ion-card-subtitle>-</ion-card-subtitle>
-			{/if}
-		</ion-card-header>
-		<ion-card-content>
-			{#if stats.grade}
+		{/if}
+	</ion-card-header>
+	<ion-card-content>
+		{#if stats.grade}
 			<canvas id="statChart" />
 			<ion-list>
 				{#if stats.top10Percent}
-					<centered-item lines ="full">
-						<ion-text slot="end">{$t("course.top10Percent")}</ion-text>
-					</centered-item>	
-				{/if}	
-				<ion-item lines ="full">
-					<ion-label>{$t("course.graded_students")}</ion-label>
+					<centered-item lines="full">
+						<ion-text slot="end">{$t('course.top10Percent')}</ion-text>
+					</centered-item>
+				{/if}
+				<ion-item lines="full">
+					<ion-label>{$t('course.graded_students')}</ion-label>
 					<ion-text slot="end">{stats.totalStudents}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.same_grade")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.same_grade')}</ion-label>
 					<ion-text slot="end">{stats.studentsLikeMe}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.better_grade")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.better_grade')}</ion-label>
 					<ion-text slot="end">{stats.studentsBetterThanMe}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.worse_grade")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.worse_grade')}</ion-label>
 					<ion-text slot="end">{stats.studentsWorseThanMe}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.passed")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.passed')}</ion-label>
 					<ion-text slot="end">{stats.passedCount}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.failed")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.failed')}</ion-label>
 					<ion-text slot="end">{stats.failedCount}</ion-text>
 				</ion-item>
-				<ion-item lines ="full">
-					<ion-label>{$t("course.average")}</ion-label>
+				<ion-item lines="full">
+					<ion-label>{$t('course.average')}</ion-label>
 					<ion-text slot="end">{stats.averageGrade}</ion-text>
 				</ion-item>
 				<ion-item lines="none">
-					<ion-label>{$t("course.successfull_average")}</ion-label>
+					<ion-label>{$t('course.successfull_average')}</ion-label>
 					<ion-text slot="end">{stats.averageGradePassed}</ion-text>
 				</ion-item>
 			</ion-list>
 		{:else}
 			<ion-list>
 				<ion-item>
-					<ion-text class="ion-padding" color="danger"
-						>{$t("course.no_stats")}</ion-text
-					>
+					<ion-text class="ion-padding" color="danger">{$t('course.no_stats')}</ion-text>
 				</ion-item>
 			</ion-list>
 		{/if}
-	
 	</ion-card-content>
-	</ion-card>
-
-	
-
+</ion-card>
 
 <style>
-
-
 	ion-text {
 		color: var(--app-color-primary-dark);
 	}
 
 	centered-item {
-    	justify-content: center;
-    	text-align: center;
+		justify-content: center;
+		text-align: center;
 		display: flex;
 	}
 
 	.teachers {
 		padding-top: 0.8rem;
 		padding-bottom: 0.8rem;
-		color: var(--app-color-primary-dark)
+		color: var(--app-color-primary-dark);
 	}
 
 	#number {
@@ -282,14 +272,13 @@
 	}
 
 	ion-accordion-group {
-	margin-top: 0.2rem;
+		margin-top: 0.2rem;
 	}
 
 	.title {
 		color: var(--app-color-primary-dark);
 		font-weight: bold;
 	}
-
 
 	.success {
 		color: var(--app-color-green-dark);
@@ -298,10 +287,4 @@
 	.danger {
 		color: var(--app-color-orange-dark);
 	}
-
-
-  
-
-
-	
 </style>
--- src/lib/components/notifications/notification.svelte
+++ src/lib/components/notifications/notification.svelte
@@ -1,218 +1,258 @@
 <script lang="ts">
-	import type { notification } from "./notifications";
-    import universisLogo from "$images/universis.png";
-    import elearningLogo from "$images/elearning.png";
-    import mail from "$images/mail.png";
-    import { open } from 'ionicons/icons';
-    import timeSinceDate from "$lib/globalFunctions/getTimeSinceDate";
-    import DOMPurify, { sanitize } from 'dompurify';
-	import { onMount } from "svelte";
-    import { t, getLocale } from "$lib/i18n";
+	import type { notification } from './notifications';
+	import universisLogo from '$images/universis.png';
+	import elearningLogo from '$images/elearning.png';
+	import mail from '$images/mail.png';
+	import { open } from 'ionicons/icons';
+	import timeSinceDate from '$lib/globalFunctions/getTimeSinceDate';
+	import DOMPurify, { sanitize } from 'dompurify';
+	import { onMount } from 'svelte';
+	import { t, getLocale } from '$lib/i18n';
 
-    export let notification: notification;
-    let iframe: HTMLIFrameElement;
-    let inlineModalOpen = false;
-    let breakpoints = [0, 0.5, 1];
-    
-    const inlineModalDismissed = (val: any) => {inlineModalOpen = false;};
+	export let notification: notification;
+	let iframe: HTMLIFrameElement;
+	let inlineModalOpen = false;
+	let breakpoints = [0, 0.5, 1];
 
-    let content = DOMPurify.sanitize(notification.body, {SANITIZE_NAMED_PROPS: true}).trim().replace(/\s+/g, ' ').replace(/\s+/g, ' ');
+	const inlineModalDismissed = (val: any) => {
+		inlineModalOpen = false;
+	};
 
-    // Adding an event listener to the iframe to set the height of the iframe to the content of the iframe
-    onMount(() => {
-        iframe.addEventListener('load', onMailLoad);
-    });
+	let content = DOMPurify.sanitize(notification.body, { SANITIZE_NAMED_PROPS: true })
+		.trim()
+		.replace(/\s+/g, ' ')
+		.replace(/\s+/g, ' ');
 
+	// Adding an event listener to the iframe to set the height of the iframe to the content of the iframe
+	onMount(() => {
+		iframe.addEventListener('load', onMailLoad);
+	});
 
-    // Fixing the height of the iframe
-    function fixIframeHeight() {
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 100 + 'px';
-    }
+	// Fixing the height of the iframe
+	function fixIframeHeight() {
+		if (iframe.contentWindow) {
+			iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 100 + 'px';
+		}
+	}
 
+	// Fixing the content of the iframe
+	function fixIframeContent() {
+		// Getting the body of the iframe
+		const body = iframe.contentDocument?.querySelector('html');
+		let bodyColor = '';
+		if (body) {
+			const root = document.documentElement;
+			// Check if body has the class 'dark'
+			if (document.body.classList.contains('dark')) {
+				// Get the CSS variable value from body.dark
+				bodyColor = getComputedStyle(document.body).getPropertyValue(
+					'--app-color-notification'
+				);
+			} else {
+				// Get the CSS variable value from root
+				bodyColor = getComputedStyle(root).getPropertyValue('--app-color-notification');
+			}
+			// Style reset for the iframe
+			body.style.fontFamily = 'Roboto, sans-serif';
+			body.style.overflowY = 'hidden';
+			body.style.color = bodyColor.trim();
 
-    // Fixing the content of the iframe
-    function fixIframeContent(){    
-        // Getting the body of the iframe
-        const body = iframe.contentDocument.querySelector("html");
-        let bodyColor = '';
-        if (body) {
+			// Adding a base tag to the iframe to open links in a new tab instead of inside the iframe
+			const baseTag = document.createElement('base');
+			baseTag.target = '_blank';
+			body.appendChild(baseTag);
+		}
+	}
 
-            const root = document.documentElement;
-            // Check if body has the class 'dark'
-            if (document.body.classList.contains('dark')) {
-                
-            // Get the CSS variable value from body.dark
-            bodyColor = getComputedStyle(document.body).getPropertyValue('--app-color-notification');
-        } else {
-            
-            // Get the CSS variable value from root
-            bodyColor = getComputedStyle(root).getPropertyValue('--app-color-notification');
-        }
-            // Style reset for the iframe
-            body.style.fontFamily = "Roboto, sans-serif";
-            body.style.overflowY = "hidden";
-            body.style.color = bodyColor.trim();
+	// Applying some fixes to the iframe content on the fly
+	function onMailLoad() {
+		fixIframeHeight();
 
-            // Adding a base tag to the iframe to open links in a new tab instead of inside the iframe
-            const baseTag = document.createElement('base');
-            baseTag.target = '_blank';
-            body.appendChild(baseTag);
-        }
-    }
+		fixIframeContent();
 
-
-    // Applying some fixes to the iframe content on the fly
-    function onMailLoad(){
-        fixIframeHeight()
-        
-        fixIframeContent();
-
-        // Janky fix for a race condition where the iframe height is not set correctly
-        setTimeout(() => {
-            fixIframeHeight();
-        }, 1000);
-
-    }
+		// Janky fix for a race condition where the iframe height is not set correctly
+		setTimeout(() => {
+			fixIframeHeight();
+		}, 1000);
+	}
 </script>
 
+<div
+	aria-hidden
+	on:click={() => {
+		inlineModalOpen = true;
+	}}
+	class="card-link"
+>
+	<div class="top">
+		<img
+			alt="Service logo"
+			src={notification.type == 'universis'
+				? universisLogo
+				: notification.type == 'webmail'
+				? mail
+				: elearningLogo}
+		/>
+		<ion-label class="notification-label sender">
+			<p>{notification.sender}</p>
+		</ion-label>
+		<p>-</p>
+		<ion-label class="notification-label date">
+			<p>{timeSinceDate(notification.dateReceived, getLocale())}</p>
+		</ion-label>
 
-<div aria-hidden on:click ={() => {inlineModalOpen = true}} class="card-link">
-    <div class="top">
-            <img alt="Service logo" src={notification.type == "universis" ? universisLogo : notification.type == "webmail" ? mail : elearningLogo} />
-            <ion-label class="notification-label sender">
-                <p>{notification.sender}</p>
-              </ion-label>
-              <p>-</p>
-              <ion-label class="notification-label date">
-                <p>{timeSinceDate(notification.dateReceived, getLocale())}</p>
-              </ion-label>
-              
-            
-  
-            <!-- <ion-label><p>{notification.sender} - {timeSinceDate(notification.dateReceived)}</p></ion-label> -->
-    </div>
-    <ion-item lines="none" class="ion-no-padding">
-        <ion-label>
-            <h2 class="ellipse-no-wrap">{notification.subject}</h2>
-        </ion-label>
-    </ion-item>
+		<!-- <ion-label><p>{notification.sender} - {timeSinceDate(notification.dateReceived)}</p></ion-label> -->
+	</div>
+	<ion-item lines="none" class="ion-no-padding">
+		<ion-label>
+			<h2 class="ellipse-no-wrap">{notification.subject}</h2>
+		</ion-label>
+	</ion-item>
 </div>
 
 <!-- Modal which displays when clicked, showing the detailed contents of the notification -->
 <ion-modal
-      is-open={inlineModalOpen}
-      initial-breakpoint={0.5}
-      {breakpoints}
-      handle-behavior="cycle"
-      on:ionModalDidDismiss={inlineModalDismissed}>
-      <ion-content >
-            <div class="mainContainer">
-                <ion-item-group>
-                    <ion-item lines="none">
-                        <ion-text>
-                            <h2>{notification.subject}</h2>
-                        </ion-text>
-                    </ion-item>
-                    <div class="context-chips ion-padding-horizontal">
-                        <ion-chip style="flex-shrink: 2;" outline color={notification.type == "universis" ? "tertiary" : notification.type == "webmail" ? "secondary" : "warning"}>
-                            <ion-avatar>
-                                <img alt="Service logo" src={notification.type == "universis" ? universisLogo : notification.type == "webmail"? mail : elearningLogo} />
-                            </ion-avatar>
-                            <ion-label style="overflow:hidden; white-space: nowrap;">
-                                {#if notification.email}
-                                    <a href="mailto:{notification.email}" style="text-decoration: none; color: inherit;">
-                                        {notification.sender}
-                                    </a>
-                                {:else}
-                                    {notification.sender}
-                                {/if}
-                            </ion-label>
-                        </ion-chip>
-                        {#if notification.url}
-                            <ion-chip color="primary" on:click={()=>{window.location = notification.url}} aria-hidden>
-                                <ion-icon icon={open} ></ion-icon>
-                                <ion-label>{$t('notifications.open')}</ion-label>
-                            </ion-chip>
-                        {/if}
-                    </div>
-                    <ion-item-divider>
-                        <ion-label>{$t('notifications.description')} </ion-label>
-                      </ion-item-divider>                    
-                    <ion-item lines="none" class="item-text-wrap ion-no-padding">
-                        <iframe bind:this={iframe}
-                        title="Body"
-                        srcdoc={content}
-                        src="about:blank"
-                        style="width: 100%; border: none;"/>
-                    </ion-item>
-                    <ion-item-divider >
-                        <ion-label>{$t('notifications.date')}: {notification.dateReceived.toLocaleString()}</ion-label>
-                      </ion-item-divider> 
-                </ion-item-group>
-            </div>
-        </ion-content>
-    </ion-modal>
+	is-open={inlineModalOpen}
+	initial-breakpoint={0.5}
+	{breakpoints}
+	handle-behavior="cycle"
+	on:ionModalDidDismiss={inlineModalDismissed}
+>
+	<ion-content>
+		<div class="mainContainer">
+			<ion-item-group>
+				<ion-item lines="none">
+					<ion-text>
+						<h2>{notification.subject}</h2>
+					</ion-text>
+				</ion-item>
+				<div class="context-chips ion-padding-horizontal">
+					<ion-chip
+						style="flex-shrink: 2;"
+						outline
+						color={notification.type == 'universis'
+							? 'tertiary'
+							: notification.type == 'webmail'
+							? 'secondary'
+							: 'warning'}
+					>
+						<ion-avatar>
+							<img
+								alt="Service logo"
+								src={notification.type == 'universis'
+									? universisLogo
+									: notification.type == 'webmail'
+									? mail
+									: elearningLogo}
+							/>
+						</ion-avatar>
+						<ion-label style="overflow:hidden; white-space: nowrap;">
+							{#if notification.email}
+								<a
+									href="mailto:{notification.email}"
+									style="text-decoration: none; color: inherit;"
+								>
+									{notification.sender}
+								</a>
+							{:else}
+								{notification.sender}
+							{/if}
+						</ion-label>
+					</ion-chip>
+					{#if notification.url}
+						<ion-chip
+							color="primary"
+							on:click={() => {
+								window.location.href = notification.url;
+							}}
+							aria-hidden
+						>
+							<ion-icon icon={open} />
+							<ion-label>{$t('notifications.open')}</ion-label>
+						</ion-chip>
+					{/if}
+				</div>
+				<ion-item-divider>
+					<ion-label>{$t('notifications.description')} </ion-label>
+				</ion-item-divider>
+				<ion-item lines="none" class="item-text-wrap ion-no-padding">
+					<iframe
+						bind:this={iframe}
+						title="Body"
+						srcdoc={content}
+						src="about:blank"
+						style="width: 100%; border: none;"
+					/>
+				</ion-item>
+				<ion-item-divider>
+					<ion-label
+						>{$t('notifications.date')}: {notification.dateReceived.toLocaleString()}</ion-label
+					>
+				</ion-item-divider>
+			</ion-item-group>
+		</div>
+	</ion-content>
+</ion-modal>
 
-    <style>
-        .ellipse-no-wrap {
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-        }
+<style>
+	.ellipse-no-wrap {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
 
-        .mainContainer {
-            padding: 0.5rem;
-        }
+	.mainContainer {
+		padding: 0.5rem;
+	}
 
-        .date {
-            font-size: smaller !important;
-        }
+	.date {
+		font-size: smaller !important;
+	}
 
-        ion-chip {
-            margin: 0rem !important;
-        }
+	ion-chip {
+		margin: 0rem !important;
+	}
 
-        .context-chips{
-            display: flex;
-            flex-direction: row;
-            gap: 1rem;
-        }
+	.context-chips {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+	}
 
-        .top {
-            display: flex;
-            align-items: center;
-            vertical-align: middle;
-            gap: 0.5rem;
-            height: 1rem;
-        }
+	.top {
+		display: flex;
+		align-items: center;
+		vertical-align: middle;
+		gap: 0.5rem;
+		height: 1rem;
+	}
 
-        .top img {
-            object-fit: contain;
-            height: 100%;
-        }
+	.top img {
+		object-fit: contain;
+		height: 100%;
+	}
 
-        .top p {
-            margin: 0;
-        }
+	.top p {
+		margin: 0;
+	}
 
-        .card-link {
-            text-decoration: none;
-        }
+	.card-link {
+		text-decoration: none;
+	}
 
-        .notification-label {
-            white-space: nowrap;
-            display: block; 
-        }
+	.notification-label {
+		white-space: nowrap;
+		display: block;
+	}
 
-        .sender {
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: inline-block;
-            max-width: calc(100% - 80px); /* Adjust as needed */
-        }
+	.sender {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: inline-block;
+		max-width: calc(100% - 80px); /* Adjust as needed */
+	}
 
-        .date p {
-            white-space: nowrap;
-        }
-
+	.date p {
+		white-space: nowrap;
+	}
 </style>
