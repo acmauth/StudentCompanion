419b119a3462b833562fcf4c2e9b46a993857a95
Christos Balaktsis <balaktsis@csd.auth.gr>
2026-02-02 00:52:40 +0200

    Modified recents

--- src/lib/translations/translations.js
+++ src/lib/translations/translations.js
@@ -241,7 +241,8 @@ export default {
 		'customCourse.title': 'Έξτρα μάθημα',
 		'customCourse.coefficient': 'Συντελεστής',
 		"homepage.today": "Σήμερα",
-		"homepage.tomorrow": "Αύριο"
+		"homepage.tomorrow": "Αύριο",
+		"recentgrades.nonews": "Δεν υπάρχουν νέα."
 	},
 	en: {
 		'menu.title': 'Club menu',
@@ -507,6 +508,7 @@ export default {
 		'customCourse.title': 'Extra course',
 		'customCourse.coefficient': 'Coefficient',
 		"homepage.today": "Today",
-		"homepage.tomorrow": "Tomorrow"
+		"homepage.tomorrow": "Tomorrow",
+		"recentgrades.nonews": "Nothing new."
 	}
 };
