af57be76522506bb9c7b0167946dba4da1f229ea
Christos Balaktsis <balaktsis@csd.auth.gr>
2026-02-28 01:28:10 +0200

    Fixed bug on webmail alert

--- src/lib/components/webmailLogin/webmailplaceholderComponent/webmailPlaceholder.svelte
+++ src/lib/components/webmailLogin/webmailplaceholderComponent/webmailPlaceholder.svelte
@@ -3,14 +3,9 @@
     import lightIcon from "./icon_light.png";
 	import { webmailLoggedIn as autheticationFlag} from '$components/webmailLogin/userCredsFlagStore';
 	import { showLoginAlert } from "../credentialLogin";
-	import Config from "$src/app.config";
-
-    let inlineModalOpen = false;
-    let breakpoints = [0, 0.5, 1];
-
 </script>
 
-{#if !$autheticationFlag && (Config.isAndroid || Config.isDevelopment)}
+{#if !$autheticationFlag}
     <ion-card href="" aria-hidden on:click ={showLoginAlert}>
         <div class="mainContents">
             <img src={darkIcon} alt="Dark Icon" class="icon darkIcon"/>
